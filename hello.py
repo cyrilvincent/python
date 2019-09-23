83a5584880fa5a966c84bac283c4eebcf9a06778
cyrilvincent <contact@cyrilvincent.com>
2019-09-23 11:46:41 +0200

    TP1

---
+++ hello.py
@@ -0,0 +1,10 @@
+print("Hello World 2!")
+
+s = "toto"
+s = s.upper()
+print(s)
+
+print(f"Bonjour {s}")
+
+i: int = "toto"
+
---
+++ tp1.py
@@ -0,0 +1,8 @@
+# 1/ Créer la variable year = 2019
+# 2/ Saisir votre année de naissance
+# 3/ Afficher votre age
+
+year = 2019
+birth = int(input("Birth date:"))
+age = year - birth
+print(f"Your age is {age}")
