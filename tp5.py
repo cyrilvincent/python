e5545ed00b7d2454d128c37db0af8fff04b3b550
cyrilvincent <contact@cyrilvincent.com>
2020-02-03 16:07:40 +0100

    Vicat 0.75/3

---
+++ tp5.py
@@ -0,0 +1,22 @@
+a = 3
+b = a
+a += 1
+print(a, b)
+
+l1 = [1,2,3]
+l2 = l1
+l1.append(4)
+print(l1, l2)
+
+l1 = [1,2,3]
+l2 = list(l1)
+l1.append(4)
+print(l1, l2)
+
+l1 = [1,2,3]
+l2 = [1,2,3]
+print(l1 == l2)
+print(l1 is l2)
+l1 = l2
+print(l1 == l2)
+print(l1 is l2)
