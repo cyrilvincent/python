12574bf1099b9a66d7954b97d8d9eba02c828b5d
cyrilvincent <contact@cyrilvincent.com>
2020-09-14 11:51:57 +0200

    ATP

---
+++ tp2.py
@@ -0,0 +1,4 @@
+# Afficher les nombres pairs de 0 à 100
+# Afficher un compte à rebours de 10 à 0 inclus
+# Saisir un nombre et afficher s'il est pair ou impair ( x % 2 == 0 => True ou False)
+# Saisir un chiffre et dire s'il est premier ou non
