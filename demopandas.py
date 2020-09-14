ec7a7408cc1d73070c820115af283f3b65b7359c
cyrilvincent <contact@cyrilvincent.com>
2020-09-14 08:59:22 +0200

    ATP

--- demopandas.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import sqlite3
-#df = pd.read_csv("data/house.csv")
-with sqlite3.connect("data/house.db3") as conn:
-    df = pd.read_sql("select * from house", conn)
-print(df.head())
-#print(df.loyer)
--- tp2.py
+++
@@ -1,4 +0,0 @@
-import tp1 as tp
-
-if __name__ == '__main__':
-    print(tp.isPrime(7))
