c7ad03580a1d82fcecf3035b61627b3d0919e8dc
cyrilvincent <contact@cyrilvincent.com>
2020-09-14 16:05:19 +0200

    ATP

--- tp5.py
+++ tp5.py
@@ -1,10 +1,52 @@
-l = [1,2,5,99,-8,54,53,6,7,98]
+import libs.tp3 as mylib
+
+l = [1,2,5,99,8,54,53,6,7,98]
 
 def filter_even(l):
     res = []
     for val in l:
-        if val % 2 == 0:
+        if mylib.is_even(val):
+            res.append(val)
+    return res
+
+def filter_prime(l):
+    res = []
+    for val in l:
+        if mylib.is_prime(val):
+            res.append(val)
+    return res
+
+def filter_generic(fn, l):
+    res = []
+    for val in l:
+        if fn(val):
             res.append(val)
     return res
 
 print(filter_even(l))
+print(filter_prime(l))
+print(filter_generic(mylib.is_even, l))
+print(filter_generic(mylib.is_prime, l))
+print(list(filter(mylib.is_even, l)))
+print(list(filter(mylib.is_prime, l)))
+print(list(filter(lambda x : x % 2 == 0, l)))
+print(list(filter(lambda x : mylib.is_prime(x) == 0, l)))
+
+def inc(l):
+    res = []
+    for val in l:
+        res.append(val + 1)
+    return res
+
+def square(l):
+    res = []
+    for val in l:
+        res.append(val ** 2)
+    return res
+
+print(inc(l))
+print(square(l))
+
+# Créer map_generic
+# Tester map : afficher les carrés de la liste
+# Tester map + filter : afficher les carrés des nombres premiers de la liste
