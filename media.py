5694d079e0fb464a9dd352ffa4236a9879effdc8
cyrilvincent <contact@cyrilvincent.com>
2020-09-15 14:15:15 +0200

    None

--- media.py
+++ media.py
@@ -1,2 +1,15 @@
-# Créer la classe Book
-# getNetPrice
+class Book:
+
+    def __init__(self, title, price, publisher=None, category=None, format="A5"):
+        self.title = title
+        self.price = price
+        self.publisher = publisher
+        self.category = category
+        self.format = format
+
+    def getNetPrice(self):
+        return self.price * 1.055
+
+if __name__ == '__main__':
+    b1 = Book("Python pour les nuls",10)
+    print(b1.getNetPrice())
