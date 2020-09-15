2ae3684d30127664c626b113a6c236197a91e2d5
cyrilvincent <contact@cyrilvincent.com>
2020-09-15 15:00:08 +0200

    None

--- geometry.py
+++ geometry.py
@@ -1,8 +1,17 @@
+class Point:
+
+    def __init__(self,x = 0, y = 0):
+        self.x = x
+        self.y = y
+
 class Rectangle:
 
-    def __init__(self, length=0, width=0):
+    nb = 0
+
+    def __init__(self, length=0, width=0, origin=Point()):
         self.length = length
         self.width = width
+        self.origin = origin
 
     def area(self):
         return self.length * self.width
@@ -10,10 +19,19 @@ class Rectangle:
     def perimeter(self):
         return 2 * (self.length + self.width)
 
+    def move(self, point):
+        self.origin = point
+
 if __name__ == '__main__':
-    r1 = Rectangle(3,2)
-    print(r1.length)
+    p1 = Point(4,-3)
+    r1 = Rectangle(3,2,p1)
     print(r1.area())
     print(r1.perimeter())
-    r2 = Rectangle(3,2)
+    r2 = Rectangle(3,2,Point(5,-8))
     print(r1 is r2)
+    print(Rectangle.nb)
+    Rectangle.nb += 1
+    print(Rectangle.nb)
+    print(r1.origin.x, r1.origin.y)
+    r1.move(Point(-1,-2))
+    print(r1.origin.x, r1.origin.y)
--- media.py
+++ media.py
@@ -1,15 +1,36 @@
+class Publisher:
+    pass
+
+class Cart:
+    pass
+
 class Book:
 
-    def __init__(self, title, price, publisher=None, category=None, format="A5"):
+    nb = 0
+
+    def __init__(self, title, price, authors = [], publisher=None, category=None, format="A5"):
         self.title = title
         self.price = price
+        self.authors = authors
         self.publisher = publisher
         self.category = category
         self.format = format
+        Book.nb += 1
 
     def getNetPrice(self):
         return self.price * 1.055
 
+    def __del__(self):
+        Book.nb -= 1
+
 if __name__ == '__main__':
-    b1 = Book("Python pour les nuls",10)
+    b1 = Book("Python pour les nuls",10,["Cyril Vincent"])
+    b1.authors.append("Toto")
     print(b1.getNetPrice())
+    print(b1.authors)
+    print(Book.nb)
+    b2 = Book("Python pour les nuls",10,["Cyril Vincent"])
+    print(Book.nb)
+    del(b1)
+    print(Book.nb)
+
