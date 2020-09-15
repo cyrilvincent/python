a983ab53958f94ca58dfaaea103287b78c0c11b0
cyrilvincent <contact@cyrilvincent.com>
2020-09-15 09:22:15 +0200

    ATP

---
+++ tp6.py
@@ -0,0 +1,20 @@
+l = range(10)
+
+res= []
+for val in l:
+    res.append(val ** 2)
+
+print(res)
+
+print(list(map(lambda x : x **2, l)))
+
+intention = [x ** 2 for x in l]
+print(intention)
+
+print(list(filter(lambda x : x % 2 == 0, l)))
+intention = [x for x in l if x % 2 == 0]
+print(intention)
+
+print(list(map(lambda x : x ** 2, filter(lambda x : x % 2 == 0, l))))
+intention = [x ** 2 for x in l if x % 2 == 0]
+print(intention)
