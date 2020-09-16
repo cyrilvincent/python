af9a9e4e4706342e62f6e4cd0eb2aaae3eca2aa4
cyrilvincent <contact@cyrilvincent.com>
2020-09-16 15:56:12 +0200

    None

---
+++ demoflask.py
@@ -0,0 +1,23 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def index(param):
+    return f"""<html>
+                    <body>
+                        <h1>Hello World!</h1>
+                        <br>Depuis le poste de Cyril
+                    </body>
+                </html>"""
+
+@app.route("/toto/titi/<param>")
+def toto(param):
+    return f"""<html>
+                    <body>
+                        <h1>Hello World!</h1>
+                        <br>Depuis le poste de Cyril {param}
+                    </body>
+                </html>"""
+
+app.run(host="0.0.0.0")
