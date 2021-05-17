c3c40250bd24b9b8e83a36100cbd102d1634db30
cyrilvincent <contact@cyrilvincent.com>
2021-05-17 13:35:52 +0200

    CEA

---
+++ tp3.py
@@ -0,0 +1,8 @@
+# Saisir un chiffre: nb
+# Afficher tous les nombres de 0 à nb-1
+# Afficher tous les nombres de nb à 0 inclus
+# Afficher si nb est pair ou impair
+# Afficher si nb est un nombre premier ou non
+# Un nombre divisible uniquement par 1 et lui même
+# Tout nombre est premiers sauf si <= 1 et s'il possède un diviseur entre 2 et nb - 1
+
