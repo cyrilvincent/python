ecc387cb34eb725619d7a086811bbd4f0c5be27c
cyrilvincent <contact@cyrilvincent.com>
2021-05-17 16:23:41 +0200

    CEA

--- tp5.py
+++ tp5.py
@@ -6,18 +6,12 @@ import tp4
 # Importer math et tester les fonctions de math
 # Saisir un nb et dire s'il est premier ou non en réutilisant tp4
 # Tester la différence entre import et from import en testant une collision de nom c'est à dire avoir une fonction is_prime dans tp5
-
-pi = 3.14
-math.pi
-tp4.pi
-
-def is_even(s):
-    pass
-
-x = "toto"
-
-# print(math.sqrt(2))
-print(tp4.is_even(5))
-# tp4.x != x
-# is_even()
+# Tester tp4 dans un if __name__== ...
+# Rendre le code robuste resaisir la données tant que vous être en erreur
+try:
+    nb = int(input("Saisir un nb: "))
+    result = tp4.is_prime(nb)
+    print(result)
+except ValueError as error:
+    print(f"Erreur {error}")
 
