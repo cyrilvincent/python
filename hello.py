75f643eb8183625eed8039324968b6833b1afead
cyrilvincent <contact@cyrilvincent.com>
2021-05-17 11:19:31 +0200

    CEA

--- hello.py
+++ hello.py
@@ -2,3 +2,39 @@ print("Hello World!")
 x = 1
 y = 2
 print(x + y)
+
+x = x + 1 # x = 2
+x += 1 # 3
+
+print(5 / 2) # 2.5
+print(5 // 2) # 2
+print(5 % 2) # 1
+
+print("toto\ntiti")
+print(r"toto\ntiti")
+print(r"c:\path")
+print("c:\toto")
+
+age = 12
+birth_date = 1972
+print(type(birth_date))
+s = "toto"
+print(type(s))
+s = "123"
+x = 123
+y = 3.14
+print(x + y)
+s2 = "456"
+print(s + s2)
+
+a = 2
+b = 3
+a, b = b, a
+
+age = input("Entrez votre age: ")
+age_integer = float(age) # int); float(); str()
+print(1 + age_integer)
+s = "toto"
+print(s.upper())
+print(s + " " + str(age_integer))
+print(f"{s} a {age_integer:.0f} ans")
---
+++ tp1.py
@@ -0,0 +1,5 @@
+# Créer tp1.py
+# Créer la variable current_year = 2021
+# Saisir votre année de naissance
+# Calculer votre age (au 31/12)
+# L'afficher
