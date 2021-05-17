cdea05939cad38527d203da4dcf620093114d4c6
cyrilvincent <contact@cyrilvincent.com>
2021-05-17 15:15:53 +0200

    CEA

--- tp4.py
+++ tp4.py
@@ -1,10 +1,22 @@
 # Faire la fonction is_even(nb) -> bool
 # Faire la fonction is_prime(nb) -> bool
 
+def add(nb, *kargs, **kwargs):
+    sum = nb
+    for value in kargs:
+        sum += value
+    return sum
+
+def add_dico(nb, **args):
+    sum = nb
+    # for key, value in args:
+    #     sum += value
+    return sum
 
 def is_even(nb: int) -> bool:
     return nb % 2 == 0
 
+is_even = lambda nb: nb % 2 == 0 # nb € E -> bool
 
 def is_prime(nb: int) -> bool:
     if nb <= 1:
@@ -17,3 +29,13 @@ def is_prime(nb: int) -> bool:
 
 print(is_even(2))
 print(is_prime(2))
+print(add(1,2,3,4))
+print(add_dico(nb=0, key1 = 0, key2 = 1))
+
+# f(x) = x + 1
+def f(x):
+    return x + 1
+# <=>
+f = lambda x : x + 1
+
+print(f(3))
