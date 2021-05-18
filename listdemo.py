2bf167d2583eaacf384d9e8018147945c963e624
cyrilvincent <contact@cyrilvincent.com>
2021-05-18 09:19:17 +0200

    CEA

---
+++ listdemo.py
@@ -0,0 +1,20 @@
+l1 = [1,2,3,4,4]
+print(l1)
+empty = []
+mono = [1]
+print(l1[1:-1])
+print(len(l1))
+l1.append(5)
+l1.insert(2,99)
+print(l1.count(4))
+# l1.remove(4)
+# print(l1.reverse().index(4))
+l1.append(l1[1])
+print(l1)
+
+for value in l1:
+    print(value)
+
+for value in range(10):
+    print(value)
+
---
+++ tp6.py
@@ -0,0 +1,6 @@
+# Dans main créer la liste l1 avec 10 entiers en dur l1 = [1,9,8,3,3,7,....
+# Créer la fonction display qui affiche le contenu de la liste
+# Créer la fonction remove_all(3) efface TOUT les 3 de la liste
+# Créer la fonction sum(l) -> somme des éléments de la liste, 0 si la liste est vide
+# Créer la fonction max(l) -> max, plante si la liste est vide
+# Bonus, add(l1, l2) = l1 + l2 add([1,2], [3,4]) => [4,6]
