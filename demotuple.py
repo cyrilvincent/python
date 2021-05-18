fbc56c7d8072dc4b7e88c69468b752ab18dbb054
cyrilvincent <contact@cyrilvincent.com>
2021-05-18 13:34:10 +0200

    CEA

---
+++ demotuple.py
@@ -0,0 +1,8 @@
+def mafunc():
+    return 12,"toto"
+
+
+if __name__ == '__main__':
+    a,b = mafunc()
+    print(a, b)
+    tuple = (1,)
---
+++ tp8.py
@@ -0,0 +1,2 @@
+# Créer la fonction min_max_avg(l) => Tuple min, max ,avg avec 1 seule boucle
+# Tester
