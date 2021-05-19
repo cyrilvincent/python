599e2a931c6173a2bfd40b5800ce7438060d5b23
cyrilvincent <contact@cyrilvincent.com>
2021-05-19 09:13:08 +0200

    CEA

--- media.py
+++ media.py
@@ -1,10 +1,22 @@
+import datetime
+
+
 class Book:
 
-    # Amazon
-    pass
-    # priceHT
-    # NetPrice() TVA 5.5%
+    def __init__(self, isbn, title, price, authors=[], publisher="", weight=-1, date=datetime.datetime.now(), nbpage=0 ):
+        self.isbn = isbn
+        self.title = title
+        self.price = price
+        self.authors = authors
+        self.publisher = publisher
+        self.weight = weight
+        self.date = date
+        self.nbpage = nbpage
+
+    def net_price(self):
+        return self.price * (1 + 0.055)
+
 
 if __name__ == '__main__':
-    # Instancier des livres
-    pass
+    b1 = Book("007", "Python", 10.0)
+    print(f"Le prix est: {b1.net_price():.2f} €")
