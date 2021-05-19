fd8a8f4f08b2e87bb9b6f3ad123ed3b11b9e1eb2
cyrilvincent <contact@cyrilvincent.com>
2021-05-19 15:37:54 +0200

    CEA

--- house.py
+++ house.py
@@ -1,9 +1,14 @@
-# code
+import argparse
 
 if __name__ == '__main__':
-    pass
-    # python house.py path --verbose --type csv --separator ,
-    # Loading path
-    # File type : csv
-    # Separator : ,
-    # Verbose mode : True
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", help="Fichier à ouvrir")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Verbeux")
+    parser.add_argument("-t", "--type", help="Type de fichier à ouvrir : csv, json, xml, xlsx")
+    parser.add_argument("-s", "--separator", help="Séparateur des fichiers CSV")
+    args = parser.parse_args()
+    print(f"Loading {args.path}")
+    if args.verbose:
+        print(f"File type: {args.type}")
+        if args.type == "csv":
+            print(f"File separator: {args.separator}")
