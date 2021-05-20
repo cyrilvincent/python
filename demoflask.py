b132688aea0f95c1ac995c6dde228beb5509d159
cyrilvincent <contact@cyrilvincent.com>
2021-05-20 14:31:58 +0200

    CEA

--- demoflask.py
+++ demoflask.py
@@ -10,5 +10,24 @@ def hello():
 def ping():
     return "pong"
 
+@app.route("/html")
+def html():
+    loyers = [1000, 2000, 3000]
+    surfaces = [50, 100, 200]
+    res = """
+    <html>
+        <header><title>Python</title></header>
+        <body>
+            <h1>Hello Python</h1>
+            <table border="1" width="100%">
+                <tr><td>Loyer</td><td>Surface</td></tr>"""
+    for index, value in enumerate(loyers):
+        res += f"<tr><td>{value}</td><td>{surfaces[index]}</td></tr>"
+    res += """</table>
+        </body>
+    </html>
+    """
+    return res
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
