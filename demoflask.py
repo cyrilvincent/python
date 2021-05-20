8556d8d75828af99bfedeea9b06f5abd1d22f7ee
cyrilvincent <contact@cyrilvincent.com>
2021-05-20 16:07:03 +0200

    CEA

--- demoflask.py
+++ demoflask.py
@@ -1,30 +1,42 @@
 import flask
+import house
 
 app = flask.Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World"
+    return f"""
+<html>
+        <header><title>Python</title></header>
+        <body>
+            <p><a href="/ping">Ping</a><p>
+            <p><a href="/house">House</a><p>
+        </body>
+</html>
+"""
 
 @app.route("/ping")
 def ping():
     return "pong"
 
-@app.route("/html")
-def html():
-    loyers = [1000, 2000, 3000]
-    surfaces = [50, 100, 200]
-    min = 35
+@app.route("/house")
+def house_html():
+    model = house.HouseCsv("data/house.csv", ",")
+    model.load()
+    min, max, avg = model.min_max_avg(model.loyers_per_m2)
     res = f"""
     <html>
         <header><title>Python</title></header>
         <body>
-            <h1>Hello Python</h1>
-            <p>Min {min}</p>
+            <h1>House</h1>
+            <p>Loyers par m²</p>
+            <p>Min: {min:.0f}</p>
+            <p>Max: {max:.0f}</p>
+            <p>Moyenne: {avg:.1f}</p>
             <table border="1" width="100%">
                 <tr><td>Loyer</td><td>Surface</td></tr>"""
-    for index, value in enumerate(loyers):
-        res += f"<tr><td>{value}</td><td>{surfaces[index]}</td></tr>"
+    for index, value in enumerate(model.loyers):
+        res += f"<tr><td>{value}</td><td>{model.surfaces[index]}</td></tr>"
     res += """</table>
         </body>
     </html>
---
+++ demotk.py
@@ -0,0 +1,53 @@
+import tkinter
+import house
+
+# Saisir dans l'entry data/house.csv
+# Dans le label bleu self.labelVariable.set(...) afficher la moyenne
+
+class simpleapp_tk(tkinter.Tk):
+    def __init__(self, parent):
+        tkinter.Tk.__init__(self, parent)
+        self.parent = parent
+        self.initialize()
+
+    def initialize(self):
+        self.grid()
+        self.entryVariable = tkinter.StringVar()
+        self.entry = tkinter.Entry(self,textvariable=self.entryVariable)
+        self.entry.grid(column=0,row=0,sticky='EW')
+        self.entry.bind("<Return>", self.OnPressEnter)
+        self.entryVariable.set("Input")
+
+        button = tkinter.Button(self,text="Search",
+                                command=self.OnButtonClick)
+        button.grid(column=1,row=0)
+
+        self.labelVariable = tkinter.StringVar()
+        label = tkinter.Label(self,textvariable=self.labelVariable,
+                              anchor="w",fg="white",bg="blue")
+        label.grid(column=0,row=1,columnspan=2,sticky='EW')
+        self.labelVariable.set("...")
+
+        self.grid_columnconfigure(0,weight=1)
+        self.resizable(True,False)
+        self.update()
+        self.geometry(self.geometry())
+        self.entry.focus_set()
+        self.entry.selection_range(0, tkinter.END)
+
+    def OnButtonClick(self):
+        
+        content = self.entryVariable.get()
+
+        self.labelVariable.set( "Hello" + content + " (button)" )
+        self.entry.focus_set()
+        self.entry.selection_range(0, tkinter.END)
+
+    def OnPressEnter(self,event):
+        self.labelVariable.set( "Hello" + " (ENTER)" )
+        self.entry.selection_range(0, tkinter.END)
+
+if __name__ == "__main__":
+    app = simpleapp_tk(None)
+    app.title('my application')
+    app.mainloop()
