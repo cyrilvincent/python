bd93ea5d65e1b510ed4a6d941bfb9a16e9a8a6dc
cyrilvincent <contact@cyrilvincent.com>
2021-05-21 15:36:19 +0200

    CEA

---
+++ demoia.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas
+import matplotlib.pyplot as plt
+import sklearn.neural_network as nn
+import sklearn.model_selection as ms
+import sklearn.preprocessing as preprocess
+
+data = pandas.read_csv("data/cancer.csv", index_col="id")
+y = data.diagnosis
+x = data.drop("diagnosis", axis=1)
+
+scaler = preprocess.StandardScaler()
+scaler.fit(x)
+x = scaler.transform(x)
+
+np.random.seed(0)
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x,y,train_size=0.8, test_size=0.2)
+
+model = nn.MLPClassifier(hidden_layer_sizes=(30,20))
+model.fit(xtrain, ytrain)
+predict = model.predict(xtest) - ytest
+print(predict.values)
+print(model.score(xtest, ytest))
