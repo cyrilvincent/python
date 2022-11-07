4a1f6c92cce6dbdac6de96d22ac92ec81402f4c2
Cyril Vincent <contact@cyrilvincent.com>
2022-11-07 09:20:00 +0100

    Teledyne

--- demore.py
+++
@@ -1,9 +0,0 @@
-import re
-
-pattern = "a(bc)+"
-value = "abcbcbcbcbcbc"
-
-if re.match(pattern, value):
-    print("OK")
-else:
-    print("KO")
--- demotuple.py
+++
@@ -1,8 +0,0 @@
-def mafunc():
-    return 12,"toto"
-
-
-if __name__ == '__main__':
-    a,b = mafunc()
-    print(a, b)
-    tuple = (1,)
