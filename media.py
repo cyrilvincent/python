87cbb136ee49393272cd2efdce41bdfa66e44f6b
Cyril Vincent <contact@cyrilvincent.com>
2022-11-08 16:44:14 +0100

    Teledyne

--- media.py
+++ media.py
@@ -20,31 +20,71 @@ class Author:
     last_name: str
     id: int
 
-class Book:
+class Media:
 
-    vat = 0.05
+    vat = 0.2
 
     def __init__(self, title: str, id: int, price: float, publisher: Publisher, date: date = date(2022,11,8), category:int = 0, lang:str="fr-FR", authors:List[Author]=[]):
         self.title = title
         self.id = id
-        self._price = price
+        self.price = price
         self.date = date
         self.category = category
         self.lang = lang
         self.publisher = publisher
         self.authors = authors
 
-    def net_price(self):
-        return self._price * (1 + Book.vat)
+    def get_net_price(self):
+        return self.price * (1 + Media.vat)
+
+class Book(Media):
+
+    vat = 0.055
+
+    def __init__(self, title: str, id: int, price: float, publisher: Publisher, date: date = date(2022, 11, 8),
+                 category: int = 0, lang: str = "fr-FR", authors: List[Author] = [], nb_page=0):
+        super().__init__(title, id,price,publisher,date,category,lang,authors)
+        self.nb_page = nb_page
+
+    def get_net_price(self):
+        return self.price * (1 + Book.vat)
+
+class Cd(Media):
+
+    def __init__(self, title: str, id: int, price: float, publisher: Publisher, date: date = date(2022, 11, 8),
+                 category: int = 0, lang: str = "fr-FR", authors: List[Author] = [], nb_track=0):
+        super().__init__(title, id,price,publisher,date,category,lang,authors)
+        self.nb_track = nb_track
+
+class Cart:
+
+    def __init__(self, id:int, medias:List[Media] = []):
+        self.id = id
+        self.medias = medias
+
+    def get_total_net_price(self):
+        return sum([m.get_net_price() for m in self.medias])
+        # sum = 0
+        # for m in self.medias:
+        #     sum += m.get_net_price()
+        # return sum
+
+    def add(self, m:Media):
+        self.medias.append(m)
+
+    def remove(self, m: Media):
+        self.medias.remove(m)
 
 if __name__ == '__main__':
     p1 = Publisher("E2V", "St Egreve")
     a1 = Author("Cyril", "Vincent", 123)
-    b1 = Book("Python",123,20.99, p1, date(2022, 11, 8))
+    b1 = Book("Python",123,20, p1, date(2022, 11, 8))
     print(b1)
     print(b1.title)
-    b2 = Book("Python 3",123,25, p1, date(2022, 11, 8), authors=[a1])
-    b1._price += 1
-    b1.net_price()
-    Book.net_price(b1)
-    Book.vat = 0.055
+    cd1 = Cd("Johnny",123,10, p1, date(2022, 11, 8), authors=[a1])
+    cart = Cart(123)
+    cart.add(b1)
+    cart.add(cd1)
+    print(cart.get_total_net_price())
+
+
