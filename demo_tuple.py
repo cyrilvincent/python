39372b43bb337b791f9fd6aa45eacfc51eeab365
Cyril Vincent <contact@cyrilvincent.com>
2022-11-08 10:46:52 +0100

    Teledyne

---
+++ demo_tuple.py
@@ -0,0 +1,15 @@
+t = (1,2,3)
+print(t)
+
+def myfunc(x):
+    return 1,2,3
+
+t = myfunc(0)
+print(t[0])
+print(t[1])
+print(t[2])
+
+a, b, c = myfunc(0)
+
+# TP
+# Créer la fonction min_max_avg(l) -> Tuple contenant le min, max et avg
--- tp_intention.py
+++ tp_intention.py
@@ -1,3 +1,12 @@
 # Filtrer une liste par nombre premier
 # Idem puis les passer au carré
 # Le faire avec filter + map ou [intention list]
+
+import tp2
+
+res = [x ** 2 for x in range(100) if tp2.is_prime(x)]
+print(res)
+
+res = list(filter(tp2.is_prime, range(100)))
+res = list(map(lambda x: x **2, res))
+print(res)
