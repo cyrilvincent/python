8dad173efed9e1d5272d875374f5a696bd596088
Cyril Vincent <contact@cyrilvincent.com>
2022-11-08 14:23:30 +0100

    Teledyne

--- Bank.py
+++ Bank.py
@@ -1,24 +1,46 @@
+from typing import List
+
+class User:
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+class Transaction:
+
+    def __init__(self, amount):
+        self.amount = amount
+
 class Account:
 
-    def __init__(self, id, owner):
+    def __init__(self, id, owner: User, transactions: List[Transaction] = []):
         self.id = id
         self.balance = 0
         self.owner = owner
+        self.transactions = transactions
 
     def credit(self, amount):
         if amount > 0:
             self.balance += amount
+            t = Transaction(amount)
+            self.transactions.append(t)
         else:
             pass
 
     def debit(self, amount):
         if amount > 0 and self.balance >= amount:
             self.balance -= amount
+            self.transactions.append(Transaction(-amount))
         else:
             pass
 
+
+
+
+
 if __name__ == '__main__':
-    a1 = Account(123, "Cyril")
+    u1 = User("Cyril", "Vincent")
+    a1 = Account(123, u1)
     a1.credit(120)
     a1.debit(50)
     print(a1.balance)
--- media.py
+++ media.py
@@ -1,11 +1,33 @@
+from datetime import date
+
+class Publisher:
+
+    pass
+
+class Author:
+    pass
+
 class Book:
 
-    def __init__(self, title: str, id: int, price: float):
+    vat = 0.05
+
+    def __init__(self, title: str, id: int, price: float, date: date = date(2022,11,8), category:int = 0, lang:str="fr-FR"):
         self.title = title
         self.id = id
-        self.price = price
+        self._price = price
+        self.date = date
+        self.category = category
+        self.lang = lang
+
+    def net_price(self):
+        return self._price * (1 + Book.vat)
 
 if __name__ == '__main__':
-    b1 = Book("Python",123,20.99)
+    b1 = Book("Python",123,20.99, date(2022, 11, 8))
     print(b1)
     print(b1.title)
+    b2 = Book("Python 3",123,25, date(2022, 11, 8))
+    b1._price += 1
+    b1.net_price()
+    Book.net_price(b1)
+    Book.vat = 0.055
