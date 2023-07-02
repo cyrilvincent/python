3dd1ea57cf3b7f153a4b36d5acb909801212e53c
Cyril Vincent <contact@cyrilvincent.com>
2023-07-02 19:22:02 +0200

    CEA

--- demo_boucle.py
+++
@@ -1,8 +0,0 @@
-for i in range(100,0,-2):
-    print(i)
-
-nb = int(input("Saisir nombre: "))
-if nb % 2 == 0:
-    print("Pair")
-else:
-    print("Impair")
--- settings.py
+++
@@ -1,3 +0,0 @@
-book_csv_path = "data/book.csv"
-book_xml_path = "data/books.xml"
-book_xl_path = "data/books.xlsx"
