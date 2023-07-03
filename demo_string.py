de8ec936a7898994a88a144e231dcfe6361d7e2d
Cyril Vincent <contact@cyrilvincent.com>
2023-07-03 11:34:49 +0200

    CEA

---
+++ demo_string.py
@@ -0,0 +1,22 @@
+a = "hello"
+b = "world"
+print(len(a))
+print(a+" "+b)
+print(a[1])
+
+c = 1
+d = 2
+temp = c
+c = d
+d = temp
+c,d = d,c
+e = f = 3
+a = 1 + 2 + 3 + 4 \
+    + 5
+print(type(a))
+print(type(b))
+
+year = input("Saisir l'année en cours:")
+year = int(year)
+print(year)
+print(type(year))
---
+++ hello.py
@@ -0,0 +1,16 @@
+print("Hello World!")
+a = 2
+b = 3
+c = a+b
+print(c)
+d = 2**3
+print(d)
+
+i = 1
+i = i + 1
+# <=>
+i += 1
+
+my_variable = 3
+my_string = "toto"
+# my_variable + my_string
