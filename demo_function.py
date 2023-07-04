f90a9f460331f82e5320d37e955612766f9cc56b
Cyril Vincent <contact@cyrilvincent.com>
2023-07-04 09:28:19 +0200

    CEA

--- demo_function.py
+++ demo_function.py
@@ -7,8 +7,15 @@ print(res)
 res = add(i=2, j=3) # By named parameter
 print(res)
 
-def is_even(nb):
+def is_even(nb: int) -> bool:
     return nb % 2 == 0
+
+def compute_distance(x1: float, y1:float, x2=0.0, y2:float=0) -> float:
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 print(is_even(7))
 print(is_even(8))
+res = compute_distance(1,2,3,4)
+print(res)
+print(compute_distance(1,2))
+
