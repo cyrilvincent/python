d738351a064155fb5a431a5cb3b7653ccbe427c7
Cyril Vincent <contact@cyrilvincent.com>
2023-07-05 14:08:49 +0200

    CEA

--- demo_tuple.py
+++ demo_tuple.py
@@ -1,4 +1,5 @@
-from typing import Tuple
+from typing import Tuple, List
+import tp6
 
 
 def my_function_tuple() -> Tuple[int ,int]:
@@ -6,6 +7,18 @@ def my_function_tuple() -> Tuple[int ,int]:
 
 x, y = my_function_tuple()
 print(x, y)
+
+def min_max_avg(l: List[float]) -> Tuple[float, float, float]:
+    min = l[0]
+    max = l[0]
+    sum = 0
+    for val in l:
+        sum += val
+        if val < min:
+            min = val
+        if val > max:
+            max = val
+    return min, max, sum / len(l)
 
 # TP
 # Créer la fonction min_max_avg(l: List[float]) -> Tuple[float, float, float]
