67a691cea294413f9d93abd443f78a45690e0d3e
Cyril Vincent <contact@cyrilvincent.com>
2023-07-05 09:34:13 +0200

    CEA

--- demo_list_write.py
+++ demo_list_write.py
@@ -7,8 +7,9 @@ print(random.randint(0, 100))
 def generate_random_list(length: int, max: int)-> List[int]:
     l = []
     for i in range(length):
-        l.append(random.random(0, max))
+        l.append(random.randint(0, max))
     return l
 
-l1 = generate_random_list(length=100, max=1000)
-print(l1)
+if __name__ == '__main__':
+    l1 = generate_random_list(length=100, max=1000)
+    print(l1)
--- tp7.py
+++ tp7.py
@@ -1,7 +1,21 @@
+from typing import List
+import demo_list_write
+
+
+def filter_even(l: List[int]) -> List[int]:
+    res = []
+    for val in l:
+        if val % 2 == 0:
+            res.append(val)
+    return res
 
 
 if __name__ == '__main__':
     l = [1,2,3,4,5,6,7,8,9,10]
     res = filter_even(l)
     print(res) # => [2,4,6,8,10]
+    l = demo_list_write.generate_random_list(10, 100)
+    print(l)
+    res = filter_even(l)
+    print(res)
     # Tester avec une list random
