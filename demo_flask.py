01eca6f9446406d98ed67a16e1f35690e370338f
Cyril Vincent <contact@cyrilvincent.com>
2023-07-06 13:58:16 +0200

    CEA

--- demo_flask.py
+++ demo_flask.py
@@ -8,13 +8,14 @@ def root():
 
 @app.route("/html")
 def html():
-    loyers, surfaces = demo_file.parse_house_csv("")
+    loyers, surfaces = demo_file.parse_house_csv("data/house/house.csv")
     s = """
     <html><body><h1>Démo <b><i><font color="red">Red</font></i></b></h1>
     <table border="1">
+    <tr><td>Loyer</td><td>Surface</td></tr>
     """
-    for i in range(100):
-        s+=f"<tr><td>A{i}</td><td>B{i}</td></tr>"
+    for loyer, surface in zip(loyers, surfaces):
+        s+=f"<tr><td>{loyer}</td><td>{surface}</td></tr>"
     s+="""
     </table>
     </body></html>
