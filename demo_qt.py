fcfde9327d94fd9b4b2c23b2d056d55f73b4c629
Cyril Vincent <contact@cyrilvincent.com>
2023-07-06 15:23:46 +0200

    CEA

--- demo_qt.py
+++ demo_qt.py
@@ -6,7 +6,11 @@ class Ui(QtWidgets.QMainWindow): # QDialog vient de widget.class dans .ui
         super(Ui, self).__init__() # Call the inherited classes __init__ method
         uic.loadUi('ui/main.ui', self) # Load the .ui file
         self.label_2.setText("Hello")
+        self.pushButton.clicked.connect(self.pushButton_clicked)
         self.show() # Show the GUI
+
+    def pushButton_clicked(self):
+        self.label_2.setText(f"{self.lineEdit.text()} Clicked")
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
