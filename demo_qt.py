65b61f283edcf00b57599fd899ad20957b92655b
Cyril Vincent <contact@cyrilvincent.com>
2023-07-06 14:34:12 +0200

    CEA

---
+++ demo_qt.py
@@ -0,0 +1,13 @@
+from PyQt5 import QtWidgets, uic
+import sys
+
+class Ui(QtWidgets.QMainWindow): # QDialog vient de widget.class dans .ui
+    def __init__(self):
+        super(Ui, self).__init__() # Call the inherited classes __init__ method
+        uic.loadUi('ui/main.ui', self) # Load the .ui file
+        self.label_2.setText("Hello")
+        self.show() # Show the GUI
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
