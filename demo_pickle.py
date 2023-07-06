07412caa603b16c837279d0dd2f6827ddbcc6f26
Cyril Vincent <contact@cyrilvincent.com>
2023-07-06 10:10:14 +0200

    CEA

--- demo_pickle.py
+++ demo_pickle.py
@@ -1,16 +1,23 @@
 import pickle
+import json
 
-x = range(1000)
+x = list(range(1000))
 
 # Save
 with open("data/demo.pickle", "wb") as f:
     pickle.dump(x, f)
+
+with open("data/demo.json", "w") as f:
+    json.dump(x, f)
 
 x = None
 
 # Load
 with open("data/demo.pickle", "rb") as f:
     x = pickle.load(f)
+
+with open("data/demo.json", "r") as f:
+    x = json.load(f)
 
 print(x)
 
