5ea6e257941b737dde7f7ef646108f35a89946b8
Cyril Vincent <contact@cyrilvincent.com>
2023-07-07 11:19:33 +0200

    CEA

--- tp_numpy.py
+++ tp_numpy.py
@@ -4,9 +4,13 @@ import demo_file
 loyers, surfaces = demo_file.parse_house_csv("data/house/house.csv")
 
 loyers = np.array(loyers)
-print(np.median(loyers), np.mean(loyers), np.std(loyers))
+print(np.min(loyers), np.max(loyers), np.median(loyers), np.mean(loyers), np.std(loyers))
 
-# Faire les stats sur loyers & surface
-# Créer le tableau loyer_m2
+loyers_m2 = loyers / surfaces
+print(np.min(loyers_m2), np.max(loyers_m2), np.median(loyers_m2), np.mean(loyers_m2), np.std(loyers_m2))
+
+
+# Faire les stats sur loyers & surfaces
+# Créer le array loyer_m2
 # Faire les stats sur loyer_m2
 
