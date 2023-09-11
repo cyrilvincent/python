515b423a38026354252561dede9041e877842997
Cyril Vincent <contact@cyrilvincent.com>
2023-09-11 13:46:40 +0200

    CNRS

--- tp1.py
+++ tp1.py
@@ -7,3 +7,20 @@
 # Saisir votre année de naissance
 # Afficher votre age
 # tester le debugger + point d'arrêt
+
+x = 1
+x += 2
+print(x)
+
+s = "Hello World!"
+print(s[6])
+
+year = 2023
+birth = input("Année de naissance: ")
+birth_int = int(birth)
+
+age = year - birth_int
+print(f"Vous avez {age} ans")
+
+
+
