926e0bb0eeac3db6a8dfc817a0fa956b3d6f6469
Cyril Vincent <contact@cyrilvincent.com>
2023-09-11 16:31:43 +0200

    CNRS

--- tp3.py
+++ tp3.py
@@ -3,3 +3,28 @@
 # Créer la fonction is_prime(x) idem pour les nombre premier
 # A tester
 
+def power(x, y):
+    return x ** y
+
+def is_even(x):
+    if x % 2 == 0:
+        return True
+    else:
+        return False
+
+def is_prime(nb):
+    if nb < 2:
+        return False
+    else:
+        is_prime = True
+        for div in range(2, int(nb ** 0.5) + 1):
+            if nb % div == 0:
+                is_prime = False
+                break
+        return is_prime
+
+print(power(2,3))
+print(is_even(8))
+print(is_prime(7))
+
+
