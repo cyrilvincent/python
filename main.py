6887419344c32b7409b03320fb4f1f79d58d7f9f
Cyril Vincent <contact@cyrilvincent.com>
2023-09-11 11:31:16 +0200

    CNRS

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Hello world")
+a = "toto"
+b = r"to\"to"
+print(b)
+c = 123
+d = 1.02e-12
+e = """
+rgkjdrkgdjfgkl
+sldkgjdlkgjfdglk
+d;sfkgjdfmlkgjd
+"xfgdg"
+"""
+f = r"retour\nchariot"
+print(e)
+print(f)
