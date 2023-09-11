dc9f5cf691edbb67ae9dbb50896351289d17ec2d
Cyril Vincent <contact@cyrilvincent.com>
2023-09-11 08:49:54 +0200

    CNRS

--- demo_heart.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-dataframe = pd.read_csv("data/heartdisease/data_cleaned_up.csv")
-dataframe_ok = dataframe[dataframe.num == 0]
-dataframe_ko = dataframe[dataframe.num == 1]
-
-print(dataframe_ok.describe().T)
-print(dataframe_ko.describe().T)
--- demo_if.py
+++
@@ -1,8 +0,0 @@
-age = 17
-condition = age < 18
-if condition:
-    print("Mineur")
-    print("toto")
-else:
-    print("Majeur")
-
