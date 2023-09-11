09d15311ae770de68d0602ae6c20d8232973c39f
Cyril Vincent <contact@cyrilvincent.com>
2023-09-11 14:34:14 +0200

    CNRS

--- tp2.py
+++ tp2.py
@@ -3,5 +3,5 @@
 # Afficher un compte à rebours 10 .. 0
 # Afficher les multiples de 7 jusqu'à 100
 # Saisir un chiffre et dire s'il est premier
-#   Un nombre est premier (prime nimber) ssi il possède 2 diviseurs : 1 et lui-même
+#   Un nombre est premier (prime number) ssi il possède 2 diviseurs : 1 et lui-même
 #   Tout nombre n est premier sauf s'il possède un diviseur entre 2 et n -1
