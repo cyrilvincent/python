b8949ec064aee41c22b19a44c035ac9d3d8787dd
Cyril Vincent <contact@cyrilvincent.com>
2023-09-12 14:08:46 +0200

    CNRS

--- tp_intention.py
+++ tp_intention.py
@@ -4,5 +4,19 @@
 # Filtrer les cos > 0.5
 # Filtrer les fabs(tanh) < 0.98 et les * par 100
 # Filtrer les nombre premiers et appliquer une racine carrée
+import math
+import my_module
+
+l1=[1,2,38,99,7,99,25,57]
+result = [x / 2 for x in l1 if x % 2 == 1]
+print(result)
+
+result = [x for x in l1 if math.cos(x) > 0.5]
+
+result = [x * 100 for x in l1 if math.fabs(math.tanh(x)) < 0.98]
+print(result)
+
+result = [math.sqrt(x) for x in l1 if not(my_module.is_prime(x)) ]
+
 
 
