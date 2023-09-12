41df856ce4a3ab53684ee786d4b5714b7ec4da72
Cyril Vincent <contact@cyrilvincent.com>
2023-09-12 13:41:26 +0200

    CNRS

---
+++ tp_intention.py
@@ -0,0 +1,8 @@
+# Créer la liste l1=[1,2,38,99,7,99,25,57]
+# Créer des listes en intention pour
+# Filtrer les impairs et les diviser par 2
+# Filtrer les cos > 0.5
+# Filtrer les fabs(tanh) < 0.98 et les * par 100
+# Filtrer les nombre premiers et appliquer une racine carrée
+
+
