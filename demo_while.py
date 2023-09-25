2b5a4237ed74190e4578cc2809532bd3873f146c
Cyril Vincent <contact@cyrilvincent.com>
2023-09-25 15:41:35 +0200

    CEA

---
+++ demo_while.py
@@ -0,0 +1,8 @@
+i = 10
+j = 2
+while i > 0 :
+    if i % 2 == 0:
+        print(i)
+    i -= 1
+
+
---
+++ tp_while.py
@@ -0,0 +1,6 @@
+# Afficher les nb pairs de 0 à 100
+# Afficher un compte à rebours de 10 à 0 inclu
+# Saisir n (entier)
+# Calculer n! : 5! = 1 * 2 * 3 * 4 * 5
+# Dire si N est premier
+# Bonus : Tout nombre > 1 est premier sauf s'il possède un diviseur entre 2 et n-1
