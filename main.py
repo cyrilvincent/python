1c4fab513f3d6c4ef7f7b640a24228cfcae12e6d
Cyril Vincent <contact@cyrilvincent.com>
2023-09-25 10:55:29 +0200

    CEA

---
+++ main.py
@@ -0,0 +1,23 @@
+print("Hello Python")
+
+a = 1
+b = 2
+print(a+b)
+
+print(4 % 2)
+
+a = a + 1
+a += 1 # <=> a = a + 1
+b -= 1 # <=> b = b - 1
+print(a, b)
+
+s1 = "toto"
+s2 = 'titi'
+print(s1)
+print("l'apostrophe")
+print('Guillemet: "')
+print("Guillemet: \" \n et l'apostrophe")
+
+c = 1
+c += 2
+
