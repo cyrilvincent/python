dbeed8c38be36ce0ed500820578aefb252de6b99
Cyril Vincent <contact@cyrilvincent.com>
2023-09-25 14:45:23 +0200

    CEA

--- tp_if.py
+++ tp_if.py
@@ -5,3 +5,18 @@
 # < 64 = Adulte
 # < 90 = Retraité
 # Au delà = Grand age
+
+age = int(input("Age: "))
+if age < 1:
+    print("BB")
+elif age <= 10:
+    print("Enfant")
+elif age < 18:
+    print("Ado")
+elif age < 64:
+    print("Adulte")
+elif age < 90:
+    print("Retraité")
+else:
+    print("Grand age")
+
