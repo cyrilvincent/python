10f3a2dff4b8e17c9131e881599af05f34bdb795
Cyril Vincent <contact@cyrilvincent.com>
2023-09-25 13:50:34 +0200

    CEA

--- demo_string.py
+++ demo_string.py
@@ -1,3 +1,7 @@
 s = "cyril"
 s = s.upper()
 print(s.center(40))
+
+x = 1
+
+print(f"Nom: {s}, x={x}")
--- tp_intro2.py
+++ tp_intro2.py
@@ -4,5 +4,5 @@ x2 = float(input("X1: "))
 y2 = float(input("X1: "))
 
 distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
-print("(x1, y1)=(" + str(x1) + "," + str(x2) + ")")
+print(f"(x1, y1)=({x1},{y1})")
 print(distance)
