e6884ff64b4bf1c43cea0ce5094828be48d271da
Cyril Vincent <contact@cyrilvincent.com>
2023-09-25 14:28:06 +0200

    CEA

---
+++ demo_if.py
@@ -0,0 +1,7 @@
+x = 2
+y = 10
+b = False
+if x < 5 and y != 8 and not b:
+    print("Cas 1")
+else:
+    print("Cas 2")
---
+++ tp_if.py
@@ -0,0 +1,7 @@
+# Saisir votre age
+# Si < 1 = Afficher BB
+# Si < 10 = enfant
+# < 18 = Ado
+# < 64 = Adulte
+# < 90 = Retraité
+# Au delà = Grand age
