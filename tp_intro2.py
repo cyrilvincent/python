7f309a5bab1a58d5cf7aedca8d8198a56ad89fcb
Cyril Vincent <contact@cyrilvincent.com>
2023-09-25 13:58:14 +0200

    CEA

--- tp_intro2.py
+++ tp_intro2.py
@@ -1,8 +1,8 @@
 x1 = float(input("X1: "))
-y1 = float(input("X1: "))
-x2 = float(input("X1: "))
-y2 = float(input("X1: "))
+y1 = float(input("Y1: "))
+x2 = float(input("X2: "))
+y2 = float(input("Y2: "))
 
 distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
-print(f"(x1, y1)=({x1},{y1})")
-print(distance)
+print(f"(x1, y1)=({x1:.2f},{y1:.2f})")
+print(f"Distance={distance:.2f}")
