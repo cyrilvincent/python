81c39001bd311ef649e2ce27ec18a04b8f0badad
Cyril Vincent <contact@cyrilvincent.com>
2023-09-26 13:42:27 +0200

    CEA

--- demo_module.py
+++ demo_module.py
@@ -1,6 +1,14 @@
 import math
-import tp_function
+import tp_function as mylib
+import mypackage.my_sub_module as submodel
 
 print(math.sqrt(2))
 print(math.cos(math.pi))
-print(tp_function.is_prime(7))
+print(mylib.is_prime(7))
+
+def factorielle():
+    return "N'importe quoi"
+
+# from tp_function import factorielle
+#
+# factorielle()
---
+++ mypackage/my_sub_module.py
@@ -0,0 +1,2 @@
+def toto():
+    return "titi"
