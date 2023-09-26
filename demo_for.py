d28f6d9db09a015f43413f133a7721b2abc3fbf3
Cyril Vincent <contact@cyrilvincent.com>
2023-09-26 09:28:53 +0200

    CEA

--- demo_for.py
+++ demo_for.py
@@ -1,5 +1,22 @@
-for i in range(5, 10, 2):
+for i in range(0, 100, 2):
     print(i)
 
-for i in range(10, 0, -1):
+for i in range(10, -1, -1):
     print(i)
+
+n = 5
+res = 1
+for i in range(1, n + 1):
+    res *= i
+print(res)
+
+n = 2
+is_prime = True
+if n<2:
+    is_prime = False
+else:
+    for div in range(2, n):
+        if n % div == 0:
+            is_prime = False
+            break
+    print(is_prime)
