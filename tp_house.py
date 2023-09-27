8ed4e96947a69ae595b06c5d20bbe1ea3e1ed052
Cyril Vincent <contact@cyrilvincent.com>
2023-09-27 14:21:51 +0200

    CEA

--- tp_house.py
+++ tp_house.py
@@ -1,5 +1,12 @@
 # python tp_house.py data/house/house.csv --csv
-# Ouverture de data/house/house.csv en mode csv: True
+# Afficher: Ouverture de data/house/house.csv en mode csv: True
 
 import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("path", help="path du fichier à ouvrir")
+parser.add_argument("-c", "--csv", action="store_true", help="Indique si le ficher est un csv")
+args = parser.parse_args()
+print(f"Ouverture de {args.path} en mode csv: {args.csv}")
+
 
