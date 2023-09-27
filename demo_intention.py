ec063f986cb4adaef8e24eed087e6109cd59c777
Cyril Vincent <contact@cyrilvincent.com>
2023-09-27 10:28:29 +0200

    CEA

--- demo_intention.py
+++ demo_intention.py
@@ -1,3 +1,5 @@
+import tp_function
+
 l1 = [0,1,2,7,5,99,6,-2,11,10]
 
 # Map equivalent
@@ -10,4 +12,8 @@ print(res)
 
 # Filter + map equivalent
 res = [x + 1 for x in l1 if x % 2 == 0]
+print(res)
+
+
+res = [x ** 2 for x in l1 if tp_function.is_prime(x)]
 print(res)
