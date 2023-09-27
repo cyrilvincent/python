d128de33e97e5bb1f556fbbf315f5b5ba48c007f
Cyril Vincent <contact@cyrilvincent.com>
2023-09-27 08:47:14 +0200

    CEA

--- tp_list.py
+++ tp_list.py
@@ -3,3 +3,35 @@
 # Créer la fonction remove_all(l, value) remove_all([1,2,2,3], 2) => [1,3]
 # Créer la fonction min, max
 # Créer la fonction average
+
+def sum(l):
+    total = 0
+    for value in l:
+        total += value
+    return total
+
+def remove_all(l, value):
+    for i in range(l.count(value)):
+        l.remove(value)
+    return l
+
+def min(l):
+    res = l[0]
+    for value in l:
+        if value < res:
+            res = value
+    return res
+
+def min2(l1):
+    l1.sort()
+    return l1[0]
+
+l1 = [0,1,2,99,4,99,6,-2,45,10]
+res = sum(l1)
+print(res)
+res = remove_all(l1, 99)
+print(res)
+res = min(l1)
+print(res)
+res = min2(l1)
+print(res)
