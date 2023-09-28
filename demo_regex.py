bba23ab8fee5e26c589fb2bce315df7dd1379875
Cyril Vincent <contact@cyrilvincent.com>
2023-09-28 14:34:28 +0200

    CEA

--- demo_regex.py
+++ demo_regex.py
@@ -1,11 +1,13 @@
 import re
 
-regex = r"a(bc)*d[ez]?f"
-regex = r"[\w.-_\d]+@[\w.-_\d]+.\w+"
+# regex = r"a(bc)*d[ez]?f"
+# regex = r"[\w.-_\d]+@[\w.-_\d]+.\w+"
+regex = r"^\d{3}-\d-\d{4}-\d{4}-\d$"
 
 while True:
-    s = input("Sasir le mot à vérifier: ")
+    s = input("Saisir le mot à vérifier: ")
     if re.match(regex, s):
         print("OK")
+        break
     else:
         print("KO")
