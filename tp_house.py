63ffeb9431ae11cc094034ffdd0a960634a5ed2d
Cyril Vincent <contact@cyrilvincent.com>
2023-09-28 11:46:50 +0200

    CEA

--- tp_house.py
+++ tp_house.py
@@ -7,6 +7,8 @@ import pickle
 parser = argparse.ArgumentParser()
 parser.add_argument("path", help="path du fichier à ouvrir")
 parser.add_argument("-c", "--csv", action="store_true", help="Indique si le ficher est un csv")
+parser.add_argument("-p", "--pickle", action="store_true", help="Indique si le ficher est un pickle")
+
 args = parser.parse_args()
 print(f"Ouverture de {args.path} en mode csv: {args.csv}")
 
@@ -27,6 +29,11 @@ def load_csv(path):
             surfaces.append(surface)
         return loyers, surfaces
 
+def load_pickle(path):
+    with open(path, "rb") as f:
+        loyers, surfaces = pickle.load(f)
+        return loyers, surfaces
+
 if __name__ == '__main__':
     if args.csv:
         loyers, surfaces = load_csv(args.path)
@@ -35,6 +42,8 @@ if __name__ == '__main__':
     # Créer l'argument -p --pickle
     # Créer la fonction load_pickle(path)
     # Faire le elif adéquat
+    elif args.pickle:
+        loyers, surfaces = load_pickle(args.path)
     else:
         print("Error, must specify file type with --csv")
         sys.exit(1)
