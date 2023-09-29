55fc3268d47adce491b38feea001dc312e9f961a
Cyril Vincent <contact@cyrilvincent.com>
2023-09-29 11:00:46 +0200

    CEA

---
+++ demo_inheritance.py
@@ -0,0 +1,32 @@
+class Point:
+
+    def __init__(self,x, y):
+        self.x = x
+        self.y = y
+
+class Rectangle:
+
+    def __init__(self, length, width, coord):
+        self.length = length
+        self.width = width
+        self.coord = coord # Association (HAS)
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+class Square(Rectangle): # Inheritance (IS)
+
+    def __init__(self, side, coord):
+        super().__init__(side, side, coord)
+
+
+
+if __name__ == '__main__':
+    x1 = Point(2,3)
+    r1 = Rectangle(4,5, x1)
+    print(r1.area(), r1.perimeter())
+    s1 = Square(3, x1)
+    print(s1.area(), s1.perimeter())
