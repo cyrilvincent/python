7e4a3ef259a2a1aac790dd96289f1f770413d9bb
Cyril Vincent <contact@cyrilvincent.com>
2023-10-17 10:55:05 +0200

    Crocus

---
+++ tp_list.py
@@ -0,0 +1,7 @@
+# Créer une liste de 10 entiers "en dur" l1 = [1,5,99,.....]
+# Créer la fonction max(l1) -> le max
+# Créer la fonction sum(l1) -> somme
+# Créer la fonction average(l1) -> moyenne
+# Créer la fonction remove_all(l1, value) -> remover toutes les valeurs value
+# Bonus : filter_even(l1) -> retourne la liste avec le nb pairs filter_even([1,2,3,4]) => [2,4]
+# Super bonus : filter_prime(l1)
