1f9782b8f93b57f3128adc1fbdd7d4dbec01a21e
Cyril Vincent <contact@cyrilvincent.com>
2023-10-17 11:57:08 +0200

    Crocus

--- tp_list.py
+++ tp_list.py
@@ -5,3 +5,60 @@
 # Créer la fonction remove_all(l1, value) -> remover toutes les valeurs value
 # Bonus : filter_even(l1) -> retourne la liste avec le nb pairs filter_even([1,2,3,4]) => [2,4]
 # Super bonus : filter_prime(l1)
+import tp_function
+
+def max(l):
+    max=l[0]
+    for val in l:
+        if val > max:
+            max = val
+    return max
+
+def max2(l):
+    l.sort()
+    return l[-1]
+
+def sum(l):
+    total = 0
+    for val in l:
+        total += val
+    return total
+
+def average(l):
+    return sum(l) / len(l)
+
+def remove_all(l, val):
+    for _ in range(l.count(val)):
+        l.remove(val)
+    return l
+
+def filter_even(l):
+    res = []
+    for val in l:
+        if tp_function.is_even(val):
+            res.append(val)
+    return res
+
+def filter_prime(l):
+    res = []
+    for val in l:
+        if tp_function.is_prime(val):
+            res.append(val)
+    return res
+
+def filter(fn, l):
+    res = []
+    for val in l:
+        if fn(val):
+            res.append(val)
+    return res
+
+l1 = [1, 2, 4, 99, 13, -2, 99, 48, 22, 10]
+print(max2(l1))
+print(sum(l1))
+print(average(l1))
+print(remove_all(l1, 99))
+print(filter_even(l1))
+print(filter_prime(l1))
+print(filter(tp_function.is_prime, l1))
+
