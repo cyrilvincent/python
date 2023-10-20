c2bab67658dcf90adcc45c8683d926d2cbed18d2
Cyril Vincent <contact@cyrilvincent.com>
2023-10-20 09:25:58 +0200

    Crocus

--- demo_intention.py
+++ demo_intention.py
@@ -8,9 +8,9 @@ def my_function(x):
 l1 = [1, 2, 4, 99, 13, -2, 99, 48, 22, 10]
 
 
-# res = [x for x in l1 if tp_function.is_prime(x)]
-# res = [x ** 2 for x in res if tp_function.is_even(x)]
-# print(res)
+res = [x for x in l1 if tp_function.is_prime(x)]
+res = [x ** 2 for x in res if tp_function.is_even(x)]
+print(res)
 
 l2 = range(100000000000000000000000000000000000000000000000000000000)
 res = (x for x in l2 if tp_function.is_prime(x))
---
+++ tp_file.py
@@ -0,0 +1,5 @@
+# Comprendre demo_file
+# Réutiliser la fonction load_csv
+# Calculer moyenne min max
+# Avec le zip de demo_zip créer la fonction qui retourne la liste des prix au m²
+# Calculer moyenne min max
