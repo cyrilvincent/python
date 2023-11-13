893a0cf7b5976a489550f63b6703d0a6d5bad987
Cyril Vincent <contact@cyrilvincent.com>
2023-11-13 13:36:20 +0100

    ATP

---
+++ demo_list.py
@@ -0,0 +1,14 @@
+from typing import List
+
+l1 = [4,3,8,-1,3,2]
+for x in l1:
+    print(x)
+
+print(l1[1])
+
+l1.append(0)
+print(l1)
+l1.remove(3)
+def toto(l1: List[int]) -> List[int]:
+    pass
+
---
+++ tp_list.py
@@ -0,0 +1,4 @@
+# Faire la fonction sum(l), max
+# filter_even(l) -> List ex: filter_even([1,2,3,4]) => [2,4]
+# filter_prime(l) -> List ex: filter_prime([1,2,3,4,5,6,7,8]) => [2,3,5,7] Reuse
+# Bonus : filter
