36db05a2902e6f8c065043fa1dda7860c096a52e
Cyril Vincent <contact@cyrilvincent.com>
2023-11-14 13:27:56 +0100

    ATP

--- media.py
+++ media.py
@@ -1,14 +1,32 @@
+from typing import List
+
+
+class Publisher:
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+class Author:
+
+    def __init__(self, id, first_name, last_name):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+
 class Book:
 
     nb_book = 0
 
-    def __init__(self, id: str, title: str, price: float, nb_page=0, lang="fr-FR", rating=0):
+    def __init__(self, id: str, title: str, price: float, nb_page=0, lang="fr-FR", rating=0, publisher: Publisher=None, authors: List[Author] = []):
         self.id = id
         self.title = title
         self.price = price
         self._nb_page = nb_page
         self.lang = lang
         self.rating = rating
+        self.publisher = publisher
+        self.authors = authors
         Book.nb_book += 1
 
     def net_price(self):
@@ -27,7 +45,10 @@ class Book:
 
 
 if __name__ == '__main__':
-    b1 = Book("001","Python",10)
+    authors = [Author("1", "Cyril", "Vincent")]
+    b1 = Book("001","Python",10, publisher=Publisher("0", "ATP"), authors = authors)
+    print(b1.publisher.name)
+    print(b1.authors[0].first_name)
     assert b1.net_price() == 10*1.055
     assert Book.nb_book == 1
     b2 = Book("002","Numpy", 20)
