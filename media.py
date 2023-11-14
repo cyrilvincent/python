6dda53c7d8c2cce0be542a46ba988eaa6746f662
Cyril Vincent <contact@cyrilvincent.com>
2023-11-14 14:40:31 +0100

    ATP

--- media.py
+++ media.py
@@ -14,23 +14,32 @@ class Author:
         self.first_name = first_name
         self.last_name = last_name
 
-class Book:
+class Media:
 
-    nb_book = 0
+    nb_media = 0
 
-    def __init__(self, id: str, title: str, price: float, nb_page=0, lang="fr-FR", rating=0, publisher: Publisher=None, authors: List[Author] = []):
+    def __init__(self, id: str, title: str, price: float, lang="fr-FR", rating=0, publisher: Publisher=None, authors: List[Author] = []):
         self.id = id
         self.title = title
         self.price = price
-        self._nb_page = nb_page
         self.lang = lang
         self.rating = rating
         self.publisher = publisher
         self.authors = authors
-        Book.nb_book += 1
+        Media.nb_media += 1
 
     def net_price(self):
         return self.price * 1.055
+
+    def __del__(self):
+        Media.nb_media -= 1
+
+class Book(Media):
+
+    def __init__(self, id: str, title: str, price: float, lang="fr-FR", rating=0, publisher: Publisher = None,
+                 authors: List[Author] = [], nb_page=0):
+        super().__init__(id, title,price,lang,rating,publisher,authors)
+        self._nb_page = nb_page
 
     @property
     def nb_page(self):
@@ -40,9 +49,13 @@ class Book:
     def nb_page(self, value):
         self._nb_page = value
 
-    def __del__(self):
-        Book.nb_book -= 1
+class Cd(Media):
 
+    def __init__(self, id: str, title: str, price: float, lang="fr-FR", rating=0, publisher: Publisher = None,
+                 authors: List[Author] = [], nb_track=0):
+
+        super().__init__(id,title,price,lang,rating,publisher,authors)
+        self.nb_tack = nb_track
 
 if __name__ == '__main__':
     authors = [Author("1", "Cyril", "Vincent")]
@@ -50,11 +63,11 @@ if __name__ == '__main__':
     print(b1.publisher.name)
     print(b1.authors[0].first_name)
     assert b1.net_price() == 10*1.055
-    assert Book.nb_book == 1
+    assert Media.nb_media == 1
     b2 = Book("002","Numpy", 20)
-    assert Book.nb_book == 2
+    assert Media.nb_media == 2
     del(b2)
-    assert Book.nb_book == 1
+    assert Media.nb_media == 1
 
 # Créer la classe Publisher (id, name)
 # Book -1 Publisher
