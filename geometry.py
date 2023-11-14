c1724b42230ea1581487c22fafa81025369a3215
Cyril Vincent <contact@cyrilvincent.com>
2023-11-14 11:26:14 +0100

    ATP

--- geometry.py
+++ geometry.py
@@ -6,24 +6,31 @@ class Rectangle:
         :param length:
         :param width:
         """
-        self.length = length
-        self.width = width
+        self._length = length
+        self._width = width
 
+    @property
     def area(self):
         """
         Aire du rectangle
         :return:
         """
-        return self.length * self.width
+        return self._length * self._width
 
     def perimeter(self):
-        return 2 * (self.length + self.width)
+        return 2 * (self._length + self._width)
+
+    def __del__(self):
+        print("Destructor")
 
 if __name__ == '__main__':
     r1 = Rectangle(2,3)
     print(r1)
-    assert r1.area() == 6
+    assert r1.area == 6
     assert r1.perimeter() == 10
-    Rectangle.area(r1) # <=> r1.area()
+    # Rectangle.area(r1) # <=> r1.area()
     r2 = Rectangle(4,3)
-    r2.length += 1
+    r2._length += 1
+    r1 = None
+    r1 = r2
+    del(r1)
--- media.py
+++ media.py
@@ -1,3 +1,21 @@
 class Book:
 
-    # net_price() => price * 1.055
+    def __init__(self, id: str, title: str, price: float, nb_page=0, lang="fr-FR", rating=0):
+        self.id = id
+        self.title = title
+        self.price = price
+        self.nb_page = nb_page
+        self.lang = lang
+        self.rating = rating
+
+    def net_price(self):
+        return self.price * 1.055
+
+
+if __name__ == '__main__':
+    b1 = Book("001","Python",10)
+    assert b1.net_price() == 10*1.055
+
+# Passe le nb_page en private + property
+# Passe le net_price en property
+# Compte automatiquement le nb_book
