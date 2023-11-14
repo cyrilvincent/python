2860d39de8fee9175d3752eade92ba15d55324dd
Cyril Vincent <contact@cyrilvincent.com>
2023-11-14 11:49:51 +0100

    ATP

--- geometry.py
+++ geometry.py
@@ -1,6 +1,22 @@
+class Point:
+
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def move(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def move_relative(self, x: float, y: float):
+        self.x += x
+        self.y += y
+
+
+
 class Rectangle:
 
-    def __init__(self, length: float, width: float):
+    def __init__(self, length: float, width: float, origin: Point=Point(0,0)):
         """
         Rectangle
         :param length:
@@ -8,6 +24,7 @@ class Rectangle:
         """
         self._length = length
         self._width = width
+        self.origin = origin
 
     @property
     def area(self):
@@ -24,13 +41,15 @@ class Rectangle:
         print("Destructor")
 
 if __name__ == '__main__':
-    r1 = Rectangle(2,3)
+    p1 = Point(4,0)
+    r1 = Rectangle(2,3, p1)
     print(r1)
     assert r1.area == 6
     assert r1.perimeter() == 10
     # Rectangle.area(r1) # <=> r1.area()
-    r2 = Rectangle(4,3)
+    r2 = Rectangle(4,3, Point(-2, 1))
     r2._length += 1
+    r2.origin.move(1,1)
     r1 = None
     r1 = r2
     del(r1)
--- media.py
+++ media.py
@@ -1,21 +1,41 @@
 class Book:
 
+    nb_book = 0
+
     def __init__(self, id: str, title: str, price: float, nb_page=0, lang="fr-FR", rating=0):
         self.id = id
         self.title = title
         self.price = price
-        self.nb_page = nb_page
+        self._nb_page = nb_page
         self.lang = lang
         self.rating = rating
+        Book.nb_book += 1
 
     def net_price(self):
         return self.price * 1.055
+
+    @property
+    def nb_page(self):
+        return self._nb_page
+
+    @nb_page.setter
+    def nb_page(self, value):
+        self._nb_page = value
+
+    def __del__(self):
+        Book.nb_book -= 1
 
 
 if __name__ == '__main__':
     b1 = Book("001","Python",10)
     assert b1.net_price() == 10*1.055
+    assert Book.nb_book == 1
+    b2 = Book("002","Numpy", 20)
+    assert Book.nb_book == 2
+    del(b2)
+    assert Book.nb_book == 1
 
-# Passe le nb_page en private + property
-# Passe le net_price en property
-# Compte automatiquement le nb_book
+# Créer la classe Publisher (id, name)
+# Book -1 Publisher
+# Créer la class Author (id, first_name, last_name, ...)
+# Book -* Author
