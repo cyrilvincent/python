ae22e355c0479b31d2e0c73ad3576c9dd81f17a3
Cyril Vincent <contact@cyrilvincent.com>
2023-11-15 11:01:19 +0100

    ATP

---
+++ demo_file.py
@@ -0,0 +1,29 @@
+import csv
+
+# f = open("data/house/house.csv")
+# s = f.read()
+# print(s)
+#
+# with open("data/house/house.csv", "r") as f_in:
+#     with open("data/house/house.tmp", "w") as f_out:
+#         for row in f_in:
+#             row = row.strip()
+#             f_out.write(f"{row}\n")
+
+with open("data/house/house.csv", "r") as f:
+    reader = csv.DictReader(f)
+    total = 0
+    nb = 0
+    for row in reader:
+        total += float(row["loyer"])
+        nb += 1
+    avg = total / nb
+
+import pickle
+with open("data/house/house.pkl", "wb") as f:
+    pickle.dump(avg, f)
+
+avg = None
+
+with open("data/house/house.pkl", "rb") as f:
+    avg = pickle.load(f)
