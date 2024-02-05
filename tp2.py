70477779d9751c42fe35cc7467a1fd157d169b4d
Cyril Vincent <contact@cyrilvincent.com>
2024-02-05 11:54:29 +0100

    CEA

---
+++ tp2.py
@@ -0,0 +1,8 @@
+# Avec un while afficher les chiffres pairs de 0 à 100
+# Avec un for afficher les multiples de 3 de 99 à 0
+# Avec 2 while afficher la table des additions de 0 à 10
+# Avec 2 for afficher la table de multiplication de 0 à 10
+# Saisir un chiffre
+# Dire s'il et premier ou non
+# n est premier sauf s'il possède un diviseur compris entre 2 et n-1
+# BONUS : afficher les 100 premiers nombres premiers
