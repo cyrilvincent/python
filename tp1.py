db966f2b42aba669402a572c3a0d22b6574ae095
Cyril Vincent <contact@cyrilvincent.com>
2024-02-05 11:18:58 +0100

    CEA

--- tp1.py
+++ tp1.py
@@ -1,3 +1,8 @@
 # Créer la variable year = 2024
 # Saisir votre année de naissance
 # Afficher votre age (en fin d'année)
+
+year = 2024
+birth = int(input("Année de naissance: "))
+age = year - birth
+print(f"Votre age est de {age} ans")
