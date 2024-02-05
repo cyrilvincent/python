ec7a815be0af2b400ab66a51118790ebc72a8318
Cyril Vincent <contact@cyrilvincent.com>
2024-02-05 14:44:43 +0100

    CEA

---
+++ demo_function.py
@@ -0,0 +1,12 @@
+def add(i:float, j:float=0) -> float:
+    """
+    Sum
+    :param i: first param
+    :param j: second argument
+    :return: i+j
+    """
+    return i + j
+
+res = add("toto", 1)
+
+print(res)
---
+++ tp3.py
@@ -0,0 +1,4 @@
+# Créer la fonction power(x, y) qui effectue x ** y
+# is_prime(nb: int) -> bool
+# tester
+# afficher tous les nb premier < 100
