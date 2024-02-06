748b9e6054ceb5587af8032881c527c112dcd45d
Cyril Vincent <contact@cyrilvincent.com>
2024-02-06 11:21:03 +0100

    CEA

--- tp4.py
+++ tp4.py
@@ -6,3 +6,44 @@
 # filter_even([1,2,3,4]) => [2,4]
 # BONUS : filter_prime(l) => retourne une liste avec que les nombres premiers
 # filter_prime([1,2,3,4,5,6]) => [2,3,5]
+from typing import List
+import tp3
+
+
+def sum(l: List) -> float:
+    res = 0
+    for value in l:
+        res += value
+    return res
+
+def mean(l: List) -> float:
+    return sum(l) / len(l)
+
+def remove_all(l: List, x: float) -> List:
+    for i in range(l.count(x)):
+        l.remove(x)
+    return l
+
+def filter_even(l: List) -> List:
+    res = []
+    for value in l:
+        if value % 2 == 0:
+            res.append(value)
+    return res
+
+def filter_prime(l: List) -> List:
+    res = []
+    for value in l:
+        if tp3.is_prime(value):
+            res.append(value)
+    return res
+
+
+
+
+if __name__ == '__main__':
+    l = [1,2,5,99,-1,5,-10,7,99,50]
+    print(sum(l), mean(l))
+    print(remove_all(l, 99))
+    print(filter_even(l))
+    print(filter_prime(l))
