f3af29b60ccbef74e4f5bc68ff8a8db1f1b3b2e8
Cyril Vincent <contact@cyrilvincent.com>
2024-02-09 13:36:27 +0100

    CEA

---
+++ qt_ui.py
@@ -0,0 +1,18 @@
+from PyQt6 import QtWidgets, uic
+import sys
+
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ui/main.ui', self)
+        self.pushButton.clicked.connect(self.pushButton_clicked)
+        self.show()
+
+    def pushButton_clicked(self):
+        self.result_label.setText("TOTO")
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec()
