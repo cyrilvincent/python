d5bb25ab098ad950edf36ef1da94a6138fba93b7
Cyril Vincent <contact@cyrilvincent.com>
2024-04-29 16:53:21 +0200

    Mycronic

--- geometry.py
+++ geometry.py
@@ -4,13 +4,17 @@ class Rectangle:
         self.long = long
         self.larg = larg
 
+    @property
     def surface(self):
         return self.long * self.larg
 
+    @property
     def perimetre(self):
         return 2 * (self.long + self.larg)
 
 
 if __name__ == '__main__':
     r1 = Rectangle(3,2)
-    print(r1.surface(), r1.perimetre())
+    print(r1.surface, r1.perimetre)
+    # print(Rectangle.surface(r1))
+    # r1.perimetre
