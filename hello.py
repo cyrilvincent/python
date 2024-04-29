088a63717861869dc32c000ce1431a4d7f5ad35e
Cyril Vincent <contact@cyrilvincent.com>
2024-04-29 11:29:46 +0200

    Mycronic

--- hello.py
+++ hello.py
@@ -3,9 +3,17 @@ print("Hello World!")
 s = input("Saisir x: ")
 x = int(s)
 
-if x > 1:
-    for div in range(2, x):
-        if x % div == 0:
-            print("Non premier")
-            break
-    print("Premier")
+
+def is_prime(x: int) -> bool:
+    if x > 1:
+        for div in range(2, x):
+            if x % div == 0:
+                return False
+        return True
+    return False
+
+
+print(is_prime(x))
+
+
+
