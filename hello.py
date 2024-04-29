505b3e72a6ac37533c088df7b24738ee9e94d6fd
Cyril Vincent <contact@cyrilvincent.com>
2024-04-29 11:52:01 +0200

    Mycronic

--- hello.py
+++ hello.py
@@ -1,19 +1,23 @@
-print("Hello World!")
 
-s = input("Saisir x: ")
-x = int(s)
-
-
-def is_prime(x: int) -> bool:
-    if x > 1:
-        for div in range(2, x):
-            if x % div == 0:
+def is_prime(nb: int) -> bool:
+    """
+    fgfgdrhd
+    :param nb:
+    :return:
+    """
+    if nb > 1:
+        for div in range(2, nb):
+            if nb % div == 0:
                 return False
         return True
     return False
 
 
-print(is_prime(x))
+if __name__ == '__main__':
+    print("Hello World!")
+
+    s = input("Saisir x: ")
+    x = int(s)
 
 
-
+    print(is_prime(x))
---
+++ main.py
@@ -0,0 +1,2 @@
+import hello
+print(hello.is_prime(7))
