d4a33df8f6128ce13da28c8b041472f2c07a0106
Cyril Vincent <contact@cyrilvincent.com>
2024-05-27 14:14:48 +0200

    CNRS

---
+++ tp2.py
@@ -0,0 +1,3 @@
+# Saisir un nombre entier et le convertir en int => nb
+# Un nombre premier est un nombre qui a exactement 2 diviseurs : 1 et lui même
+# Tout nombre > 1 est premier sauf s'il possède un diviseur
