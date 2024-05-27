118bbc47b28ae475d9cdae36e65b50a81c0c53fc
Cyril Vincent <contact@cyrilvincent.com>
2024-05-27 11:32:25 +0200

    CNRS

---
+++ tp1.py
@@ -0,0 +1,8 @@
+# Créer la variable year = 2024
+# Saisir votre année de naissance
+# Afficher votre age en fin d'année
+# Saisir votre prénom espace nom dans la variable full_name
+# full_name.index
+# Créer la variable first_name qui contient uniquement le prénom
+# Créer la variable last_name qui contient uniquement le nom
+# Afficher
