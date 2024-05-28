201c974178dd5ecbb77ed2072d6216317a129e5b
Cyril Vincent <contact@cyrilvincent.com>
2024-05-28 13:56:49 +0200

    CNRS

---
+++ demo_dict.py
@@ -0,0 +1,2 @@
+dict = {"first_name": "Cyril", "last_name": "Vincent"}
+print(dict["first_name"])
--- demo_tuple.py
+++ demo_tuple.py
@@ -1,7 +1,24 @@
+import datetime
+import time
+
+
 def min_max_avg(l) -> tuple[float, float, float]:
-    return 0, 10, 5
+    min = l[0]
+    max = l[0]
+    sum = 0
+    for value in l:
+        if value < min:
+            min = value
+        if value > max:
+            max = value
+        sum += value
+    return min, max, sum / len(l)
 
 
-min, max, avg = min_max_avg([1,2,3])
+now = datetime.datetime.now()
+min, max, avg = min_max_avg(range(1000000))
+after = datetime.datetime.now()
+print(after - now)
+
 print(min, max, avg)
 
