0a4e477ee1746dfbb4a43a74f653db5765037d6f
Cyril Vincent <contact@cyrilvincent.com>
2024-05-28 09:57:33 +0200

    CNRS

---
+++ demo_list.py
@@ -0,0 +1,26 @@
+l = [1,5,6,8,9,0,-1,7]
+l.append(99)
+l.insert(2,88)
+print(l)
+
+for value in l:
+    print(value)
+
+for i in range(len(l)):
+    print(l[i])
+
+
+def hello(l: list[float]):
+    for val in l:
+        print(val)
+
+# 3.14 != [3.14]
+
+hello(1.2)
+
+# TP
+# Créer la fonction sum(l:list[float]) -> float
+# Créer la fonction max(l:list[float]) -> float, idem pour min
+# Créer la fonction average
+# Bonus : créer la fonction filter_even(l:List[float]) -> list[float]
+# filter_even([1,2,3,4]) -> [2,4]
