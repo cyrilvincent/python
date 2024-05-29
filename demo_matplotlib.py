2b31709f15826f5c0a712414011cd5777ffbe3e1
Cyril Vincent <contact@cyrilvincent.com>
2024-05-29 14:47:56 +0200

    CNRS

--- demo_matplotlib.py
+++ demo_matplotlib.py
@@ -11,7 +11,9 @@ plt.bar(xbar, xbar)
 plt.subplot(212)
 plt.scatter(x, y, color="green")
 plt.plot(x, y2)
+plt.savefig("data/house/house.png")
 plt.show()
+
 
 # surfaces ,loyers
 # Afficher le nuage de points surfaces / loyers
