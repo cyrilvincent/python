2d0cf9a62f96fde3a6a4a831200620cd71967e84
Cyril Vincent <contact@cyrilvincent.com>
2024-05-29 12:08:08 +0200

    CNRS

--- tp_house.py
+++ tp_house.py
@@ -27,3 +27,10 @@ if __name__ == '__main__':
     np.savez("data/house/house.npz", loyers=loyers, surfaces=surfaces, loyers_per_m2=loyers_per_m2)
     content = np.load("data/house/house.npz")
     print(content.keys())
+
+    loyer_mean = np.mean(loyers)
+    loyer_std = np.std(loyers)
+    loyers_greater_mean = loyers[loyers > loyer_mean + 3 * loyer_std]
+    surfaces_greater_mean = surfaces[loyers > loyer_mean + 3 * loyer_std]
+    print(loyers_greater_mean, surfaces_greater_mean)
+    print(list(zip(loyers_greater_mean, surfaces_greater_mean)))
