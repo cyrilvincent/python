b0cc39b4c0560af1e6831a73a11029bb9b29e918
Cyril Vincent <contact@cyrilvincent.com>
2024-06-25 08:55:54 +0200

    CEA

--- tp_function.py
+++ tp_function.py
@@ -10,15 +10,23 @@ def is_even(x: int) -> bool:
         return True
     else:
         return False
-    
+
 def is_prime(x: int) -> bool:
+    """
+    Retourne True si x est premier
+    """
+    a = 4
     if x < 2:
         return False
-    for div in range(2, x ** 0.5 + 1):
+    for div in range(2, int(x ** 0.5) + 1):
         if x % div == 0:
             return False
     return True
+
+
+if __name__ == '__main__':
+    print("TOTO")
+    print(is_even(2154679))
+    res = is_prime(13)
+    print(res)
     
-print(is_even(2154679))
-res = is_prime(6)
-print(res)
