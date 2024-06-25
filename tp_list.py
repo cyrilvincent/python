d703b03c0d372f70a17b08e3c14ac2d1dd0f7627
Cyril Vincent <contact@cyrilvincent.com>
2024-06-25 09:32:03 +0200

    CEA

---
+++ tp_list.py
@@ -0,0 +1,7 @@
+# Programmer un module
+# Tester dans le module puis tester dans un main.py
+# Refaire la fonction sum et tester
+# Faire la fonction average
+# Faire les fonctions min, max
+# Faire la fonction sum_even qui somme que les nombres pairs
+# Bonus : palindrome(l) -> bool ex: 1,2,3,2,1, ex: 1,2,3,3,2,1 1,2,3,4,2,1 n'est pas un palindrome
