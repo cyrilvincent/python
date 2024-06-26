fa167b8718464f7406df1e772d63028cd2500eec
Cyril Vincent <contact@cyrilvincent.com>
2024-06-26 09:12:38 +0200

    CEA

---
+++ data/climate/tp_numpy.py
@@ -0,0 +1,7 @@
+# Créer un tableau numpy de -pi à pi avec un pas de 0.01 : a1
+# Appliquer un sin : asin
+# Appliquer une tanh : atanh
+# Créer la fonction sigmoid (voir wikipedia)
+# Créer asigmoid
+# Sur atanh prendre les x € [-0.5, +0.5]
+# atanh - x sur [-0.5, +0.5] ~= 0 => np.max(np.abs(atanh - x))
