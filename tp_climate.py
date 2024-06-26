b110326728e03c4da27b1e555b769fc09fea715f
Cyril Vincent <contact@cyrilvincent.com>
2024-06-26 16:12:37 +0200

    CEA

--- tp_climate.py
+++ tp_climate.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # data/climate/
 # pd.read_csv(index_col="nom_col")
@@ -6,3 +7,25 @@ import pandas as pd
 # Faire un describe + pd.set_option('display.max_columns', None)
 # Afficher dans matplotlib les plots des T (degC), des p (mbar) et Tdew (degC)
 # Ajouter la colonne mist True False
+
+dataframe = pd.read_csv("data/climate/jena_filtered.csv", index_col="Date Time")
+dataframe = dataframe[11::24]
+pd.set_option("display.max_columns", None)
+print(dataframe.describe().T)
+
+dataframe["mist"] = dataframe["T (degC)"] <= dataframe["Tdew (degC)"]
+print(dataframe[dataframe["mist"]])
+
+plt.subplot(221)
+plt.plot(dataframe.index, dataframe["p (mbar)"])
+plt.xticks([])
+plt.subplot(222)
+plt.plot(dataframe.index, dataframe["T (degC)"])
+plt.xticks([])
+plt.subplot(223)
+plt.plot(dataframe.index, dataframe["Tdew (degC)"])
+plt.xticks([])
+plt.subplot(224)
+plt.plot(dataframe.index, dataframe["rh (%)"])
+plt.xticks([])
+plt.show()
