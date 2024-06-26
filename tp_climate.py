930360b028dd5405466679bb073fc07ae9d227c0
Cyril Vincent <contact@cyrilvincent.com>
2024-06-26 15:13:06 +0200

    CEA

--- tp_climate.py
+++ tp_climate.py
@@ -4,5 +4,5 @@ import pandas as pd
 # pd.read_csv(index_col="nom_col")
 # Alléger le dataframe prendre 1 ligne à midi tous les jours
 # Faire un describe + pd.set_option('display.max_columns', None)
-# Afficher dans matplotlib les nuages les plots des T (degC) et des p (mbar) et Tdew (degC)
+# Afficher dans matplotlib les plots des T (degC), des p (mbar) et Tdew (degC)
 # Ajouter la colonne mist True False
