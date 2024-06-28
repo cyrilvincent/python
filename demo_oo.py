8d9b5f853daf108aec1e98930bde0917d362815b
Cyril Vincent <contact@cyrilvincent.com>
2024-06-28 13:48:12 +0200

    CEA

--- demo_oo.py
+++ demo_oo.py
@@ -1,8 +1,19 @@
+class Coord:
+
+    def __init__(self, x: float=0, y: float=0) -> None:
+        self.x = x
+        self.y = y
+
+    def move(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
 class Rectangle:
 
-    def __init__(self, length: float, width: float) -> None:
+    def __init__(self, length: float, width: float, coord: Coord = Coord()) -> None:
         self.length = length
         self.width = width
+        self.coord = coord
 
     def area(self):
         return self.length * self.width
@@ -10,8 +21,21 @@ class Rectangle:
     def perimeter(self):
         return 2 * (self.length + self.width)
     
+class VideoGame:
+
+    def __init__(self, rectangles: list[Rectangle]=[]) -> None:
+        self.rectangles = rectangles
+
+    def score(self):
+        return sum([r.area() for r in self.rectangles])
+    
 if __name__=='__main__':
-    r1 = Rectangle(3,2)
+    c1 = Coord(-1, 1)
+    r1 = Rectangle(3,2, c1)
     print(r1.area(), r1.perimeter())
-    r2 = Rectangle(4,1)
+    r2 = Rectangle(4,1, Coord(1,2))
     # Rectangle.area(r2)
+    r2.coord.move(3,2)
+    r2.coord.x = 4
+    c00 = Coord()
+    vg = VideoGame([r1, r2])
---
+++ tp_media.py
@@ -0,0 +1,4 @@
+# class Book avec 5 à 10 attributs
+# méthode net_price() = price * 1.055
+# Un book possède un publisher (name)
+# Un book possède n authors (first_name, last_name)
