9ed3fb1d606bde6372e287b9296a01f7175dec08
Cyril Vincent <contact@cyrilvincent.com>
2024-06-28 10:19:12 +0200

    CEA

---
+++ demo_cancer.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import sklearn.ensemble as rf
+import sklearn.model_selection as ms
+import sklearn.metrics as metrics
+import matplotlib.pyplot as plt
+import sklearn.neural_network as nn
+
+dataframe = pd.read_csv("data/cancer/data.csv", index_col="id")
+print(dataframe.describe().T)
+y = dataframe.diagnosis # dataframe["diagnosis"]
+x = dataframe.drop("diagnosis", axis=1)
+
+np.random.seed(42)
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
+
+#model = rf.RandomForestClassifier(n_estimators=100)
+model = nn.MLPClassifier(hidden_layer_sizes=(30,30,30))
+model.fit(xtrain, ytrain)
+ypred = model.predict(xtest)
+score = model.score(xtrain, ytrain)
+print(score)
+score = model.score(xtest, ytest)
+print(score)
+#print(model.feature_importances_)
+
+# plt.bar(x.columns, model.feature_importances_)
+# plt.xticks(rotation=45)
+# plt.show()
+
+
+
+print(metrics.classification_report(ytest, ypred))
+print(metrics.confusion_matrix(ytest, ypred))
--- demo_curvefit.py
+++ demo_curvefit.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
+import scipy.integrate as integrate
 
 np.random.seed(0)
 noise = 1
@@ -19,7 +20,6 @@ model4 = lambda x,a,b,c,d,e: a*x**4 + b*x**3 + c*x**2 + d*x + e
 # def model(x,a,b,c,d):
 #     return a * x * np.sin(x * b) + c
 
-
 weigths, conv = opt.curve_fit(model, x, f(x))
 print(weigths)
 print(conv)
@@ -27,6 +27,12 @@ print(conv)
 weigths4, conv4 = opt.curve_fit(model4, x, f(x))
 print(weigths4)
 print(conv4)
+
+
+diff = lambda x,a,b,c,a4,b4,c4,d4,e4: np.abs(model(x,a,b,c)-model4(x,a4,b4,c4,d4,e4))
+area = integrate.quad(diff,2,4, args=(weigths[0] , weigths[1], weigths[2], weigths4[0] , weigths4[1], weigths4[2], weigths4[3], weigths4[4]))
+print(area)
+
 
 x = np.linspace(0, 10, 100)
 plt.plot(x, model(x, weigths[0] , weigths[1], weigths[2]), color="red")
