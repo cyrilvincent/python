a038fa190753d873e6b0e18ea925426bfa0e6551
Cyril Vincent <contact@cyrilvincent.com>
2024-07-09 16:52:20 +0200

    Mycronic

--- services.py
+++ services.py
@@ -1,6 +1,7 @@
 import media
 import abc
 import mock_db
+import logging
 
 class AService(abc.ABC):
 
@@ -26,7 +27,25 @@ class AService(abc.ABC):
 
 class CartService(AService):
 
-    pass
+    def __init__(self):
+        super().__init__()
+
+    def search(self, query: str) -> list[media.Media]:
+        return [m for m in self.db if query.strip().upper() in m.title.upper()]
+
+    def detail(self, id: int) -> media.Media:
+        return [m for m in self.db if m.id == id][0]
+
+    def add_to_cart(self, media: media.Media, cart: media.Cart):
+        cart.medias.append(media)
+
+    def validate(self, cart: media.Cart) -> bool:
+        if len(cart.medias) == 0:
+            raise ValueError("Can't validate an empy cart")
+        return True
+
+    def pay(self, cart: media.Cart):
+        logging.info("Payed")
 
 
 
