14fe51d0a506c5d5deb499055ac2ca9e940c1227
cyrilvincent <contact@cyrilvincent.com>
2024-09-23 13:48:05 +0200

    CEA

---
+++ demo_if.py
@@ -0,0 +1,21 @@
+a = 2
+b = 4
+if a == 2:
+    print("toto")
+    if a < 5:
+        print("titi")
+
+s = "toto"
+if "o" in s:
+    print("OK")
+
+ok = True
+
+if a % 2 == 0 and b % 2 == 0:
+    print("a et b sont pairs")
+
+condition = a % 2 == 0 and b % 2 == 0
+if condition:
+    print("a et b sont pairs")
+
+
---
+++ tp2.py
@@ -0,0 +1,20 @@
+# Condition
+# Saisir votre age et afficher votre catégroei d'age
+# < 1 an : BB
+# < 12 ans : enfant
+# < 18 ans : ado
+# < 64 ans : actif
+# < 100 ans : retraité
+# sinon : grand age
+
+# Boucle
+# Faire une boucle de 0 à 100 inclus
+# Faire un compte à rebours de 10 à 0
+# Afficher tous les multiples de 3 inférieurs à 100
+
+# Additionner tous les nombres de 1 à n, par exemple n=5 => 1+2+3+4+5 => 15
+
+# Bonus
+# Soit x un entier > 0
+# Afficher True si x est premier
+# Tous nombre x >= 2 est premier sauf s'il possède un diviseur entre 2 et x-1
