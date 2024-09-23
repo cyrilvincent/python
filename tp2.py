a3b34a3cc52aecfa67182b698c30f29f63986a94
cyrilvincent <contact@cyrilvincent.com>
2024-09-23 15:02:08 +0200

    CEA

--- tp2.py
+++ tp2.py
@@ -18,3 +18,35 @@
 # Soit x un entier > 0
 # Afficher True si x est premier
 # Tous nombre x >= 2 est premier sauf s'il possède un diviseur entre 2 et x-1
+
+age = 20
+if age < 1:
+    print("BB")
+elif age < 12:
+    print("Enfant")
+elif age < 18:
+    print("Ado")
+else:
+    print("Adulte")
+
+for i in range(11):
+    print(i)
+
+for i in range(10,-1,-1):
+    print(i)
+
+for i in range(0,10,3):
+    print(i)
+
+n = 5
+sum = 0
+for i in range(1, n+1):
+    sum = sum + i
+print(sum)
+
+x = 1223
+for div in range(2, int(x ** 0.5) + 1):
+    if x % div == 0:
+        print("Non premier")
+        break
+print("Premier")
