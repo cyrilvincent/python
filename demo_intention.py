c7bb33b0556087bf9ec51a959698aec41ac0abe3
Cyril Vincent <contact@cyrilvincent.com>
2024-09-23 08:29:07 +0200

    CEA

--- demo_intention.py
+++
@@ -1,7 +0,0 @@
-import tp_function
-
-l = range(1000000000000000000000000000000000000000000000000000000000000000000000000000000)
-res = (x for x in l if tp_function.is_prime(x))
-res = (x ** 2 for x in res if tp_function.is_even(x))
-for v in res:
-    print(v)
