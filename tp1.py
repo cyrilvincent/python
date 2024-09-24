9f208e47a23854f0d0f6c774853ab60c4379ccb0
cyrilvincent <contact@cyrilvincent.com>
2024-09-24 10:20:12 +0200

    CEA

--- tp1.py
+++ tp1.py
@@ -3,7 +3,12 @@
 # Afficher votre age au 31/12
 
 year = 2024
-birth = input("Année de naissance: ")
-birth = int(birth)
-age = year - birth
-print(f"Vous avez {age} ans")
+while True:
+    birth = input("Année de naissance: ")
+    try:
+        birth = int(birth)
+        age = year - birth
+        print(f"Vous avez {age} ans")
+        break
+    except ValueError as ve:
+        print(f"Erreur: {ve}")
