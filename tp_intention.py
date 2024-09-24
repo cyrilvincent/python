af748f6286d33c5a40a89e5c09d25981000da146
cyrilvincent <contact@cyrilvincent.com>
2024-09-24 14:12:53 +0200

    CEA

--- tp_intention.py
+++ tp_intention.py
@@ -1,7 +1,21 @@
 # Créer une liste de 10 entiers
 # Filtrer les nombres premier avec filter
 # Passer au carré tous les éléments de la liste avec un map
-# Filter les nombre premier de la liste et les monter au carré filter + map
+# Filtrer les nombre premier de la liste et les monter au carré filter + map
 # Filtrer les nombres premier avec une liste en intention
 # Passer au carré tous les éléments de la liste avec une liste en intention
-# Filter les nombre premier de la liste et les monter au carré avec une liste en intention
+# Filtrer les nombre premier de la liste et les monter au carré avec une liste en intention
+import tp3
+
+l = [1,2,5,9,99,-1,0,8,14,50]
+res = filter(tp3.is_prime, l)
+print(list(res))
+
+res = map(lambda x: x ** 2, l)
+print(list(res))
+
+res = map(lambda x: x ** 2, filter(tp3.is_prime, l))
+print(list(res))
+
+res = [x ** 2 for x in l if tp3.is_prime(x)]
+print(res)
