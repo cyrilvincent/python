dc967431ca638e7356c42f76e6cb9d3daec524b8
cyrilvincent <contact@cyrilvincent.com>
2024-09-24 11:09:10 +0200

    CEA

---
+++ tp_list.py
@@ -0,0 +1,7 @@
+# Créer une liste avec 10 entiers "en dur" non consécutifs
+# Afficher la longueur de la liste
+# Créer la fonction sum(l: list[int]) -> int
+# Créer la fonction average(l: list[int]) -> float
+# Créer la fonction max(l: list[int]) -> int
+# Créer la fonction filter_even(l: list[int]) -> list[int] : filter_even([1,2,3,4]) -> [2,4]
+# Bonus : Créer la fonction filter_prime([1,2,3,4,5,6,7,8]) -> [2,3,5,7]
