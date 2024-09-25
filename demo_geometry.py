231dfaa1338f8c6cd1ccd63c794518cccc7b2452
cyrilvincent <contact@cyrilvincent.com>
2024-09-25 09:53:10 +0200

    CEA

--- demo_geometry.py
+++ demo_geometry.py
@@ -1,9 +1,24 @@
+class Coord:
+
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def move(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def move_relative(self, x: float, y: float):
+        self.x += x
+        self.y += y
+
 class Rectangle:
 
-    def __init__(self, length: float, width: float):
+    def __init__(self, length: float, width: float, coord: Coord):
         # QUOI
         self.length = length
         self.width = width
+        self.coord = coord
 
     def get_area(self):
         return self.length * self.width
@@ -12,6 +27,10 @@ class Rectangle:
         return  2 * (self.length + self.width)
 
 if __name__ == '__main__':
-    r1 = Rectangle(3,2)
+    c1 = Coord(4,-1)
+    r1 = Rectangle(3,2, c1)
     print(r1.get_area())
     print(r1.get_perimeter())
+    r2 = Rectangle(2,4,Coord(5,2))
+    r2.coord.move_relative(1,-2)
+    print(f"Coordonnées ({r2.coord.x},{r2.coord.y})")
--- media.py
+++ media.py
@@ -1,4 +1,11 @@
+# class Publisher (id, name)
+# Book possède 1 Publisher
+# class Author (first_name, last_name)
+# Book possède n Authors (list[Author])
+
 class Book:
+
+    tva = 0.055
 
     def __init__(self, title: str, price: float, lang="fr-FR", note=0.0, nb_page=0):
         self.title = title
@@ -8,7 +15,7 @@ class Book:
         self.nb_page = nb_page
 
     def get_net_price(self):
-        return self.price * 1.055
+        return self.price * (1 + Book.tva)
 
 if __name__ == '__main__':
     b1 = Book("Python", 10)
