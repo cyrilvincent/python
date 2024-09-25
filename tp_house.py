cc15adff2be9b78a1a67af76a0cfbfbae0ced6f0
cyrilvincent <contact@cyrilvincent.com>
2024-09-25 15:07:28 +0200

    CEA

--- tp_house.py
+++ tp_house.py
@@ -6,3 +6,26 @@
 # Calculer le loyer par m2 moyen
 # Bonus : Créer le ficher CSV avec 3 colonnes : loyer,surface,loyer_per_m2
 # Bonus : OO, Fonctions
+import csv
+
+
+def load(path: str) -> tuple[list[int], list[int], list[float]]:
+    with open(path, "r") as f:
+        loyers = []
+        surfaces = []
+        loyers_per_m2 = []
+        reader = csv.DictReader(f)
+        for row in reader:
+            loyer = int(row["loyer"])
+            surface = int(row["surface"])
+            loyers.append(loyer)
+            surfaces.append(surface)
+            loyers_per_m2.append(loyer / surface)
+        return loyers, surfaces, loyers_per_m2
+
+if __name__ == '__main__':
+    loyers, surfaces, loyers_per_m2 = load("data/house/house.csv")
+    print(loyers_per_m2)
+    print(sum(loyers) / len(loyers))
+    print(sum(surfaces) / len(surfaces))
+    print(sum(loyers_per_m2) / len(loyers_per_m2))
