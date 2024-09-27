e6eb9a91034aab7f0ef7ede834dd6838f8ca9b60
cyrilvincent <contact@cyrilvincent.com>
2024-09-27 14:08:13 +0200

    CEA

--- demo_qt_designer.py
+++ demo_qt_designer.py
@@ -7,7 +7,12 @@ class Ui(qt.QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("ui/test.ui", self)
+        self.pushButton.clicked.connect(self.pushButton_clicked)
+        self.actionOpen.triggered.connect(self.pushButton_clicked)
         self.show()
+
+    def pushButton_clicked(self):
+        self.label_2.setText("Coucou")
 
 if __name__ == '__main__':
     app = qt.QApplication(sys.argv)
