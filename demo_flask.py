0899f41057850f83b151604022c09942877dcd5e
cyrilvincent <contact@cyrilvincent.com>
2024-09-27 09:28:37 +0200

    CEA

--- demo_flask.py
+++ demo_flask.py
@@ -7,5 +7,18 @@ app = flask.Flask(__name__)
 def root():
     return "Hello World!!!!!"
 
+@app.route("/toto/<x>")
+def toto(x):
+    return f"toto {x}"
+
+@app.route("/add/<x>/<y>")
+def add(x, y):
+    x = int(x)
+    y = int(y)
+    return str(x + y)
+
+# Importer tp3
+# Créer la route isprime/7 qui répond True ou False
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
--- demo_matshow.py
+++ demo_matshow.py
@@ -4,7 +4,7 @@ import imageio.v2 as imageio
 
 mat1 = np.random.rand(8,8)
 mat2 = np.random.rand(8,8)
-step = 10
+step = 100
 for k in range(step + 1):
     print(k)
     mat = np.zeros((8,8))
@@ -12,14 +12,16 @@ for k in range(step + 1):
         for col in range(8):
             mat[row,col] = mat1[row, col] + (mat2[row, col] - mat1[row, col]) * k / step
     fig, ax = plt.subplots()
-    ax.matshow(mat)
+    res = ax.matshow(mat, vmin=0, vmax=1)
     for (i, j), z in np.ndenumerate(mat):
         ax.text(j, i, '{:0.2f}'.format(z), ha='center', va='center')
-    # plt.colorbar()
+    cbar = fig.colorbar(res)
+    cbar.ax.set_ylim(0, 1)
     fig.savefig(f"data/images/mat{k}.png")
-plt.show()
+    plt.close(fig)
+# plt.show()
 
-with imageio.get_writer("data/images/animation.gif", fps=2) as writer:
+with imageio.get_writer("data/images/animation.gif", fps=5) as writer:
     for i in range(step):
         image = imageio.imread(f"data/images/mat{i}.png")
         writer.append_data(image)
