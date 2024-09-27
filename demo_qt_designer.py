dbc45c7361e7a386a24641ad184eba913f85c430
cyrilvincent <contact@cyrilvincent.com>
2024-09-27 14:34:31 +0200

    CEA

--- demo_qt_designer.py
+++ demo_qt_designer.py
@@ -9,12 +9,23 @@ class Ui(qt.QMainWindow):
         uic.loadUi("ui/test.ui", self)
         self.pushButton.clicked.connect(self.pushButton_clicked)
         self.actionOpen.triggered.connect(self.pushButton_clicked)
+        for i in range(10):
+            self.listWidget.addItem(f"Python {i}")
+        self.listWidget.currentItemChanged.connect(self.list_Widget_CurrentItemChanged)
         self.show()
 
     def pushButton_clicked(self):
         self.label_2.setText("Coucou")
 
+    def list_Widget_CurrentItemChanged(self):
+        print(self.listWidget.currentItem().text(), self.listWidget.currentRow())
+
 if __name__ == '__main__':
     app = qt.QApplication(sys.argv)
     window = Ui()
     app.exec()
+
+# Dans un module book_lib créer la fonction load(path) qui charge data/media/books.json avec le package json (json.load())
+# Créer dans qtdesigner une fenetre qui affiche la liste des livres
+# Créer un bouton add to cart
+# Créer un label qui affiche la somme des livres ajouté au panier
