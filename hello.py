3a37b1ec59b92fe05ea87ec39fa4066df68d5d52
cyrilvincent <contact@cyrilvincent.com>
2024-10-14 11:45:42 +0200

    CEA

---
+++ hello.py
@@ -0,0 +1,40 @@
+print("Hello World \u1254")
+a = 2
+b = a + 4
+print(a, b)
+age_du_capitaine = 54
+
+x = a / b
+print(x)
+
+print(type(a), type(x))
+s = "toto"
+print(type(s))
+print(s)
+
+z = "12"
+x = 12
+w = "34"
+
+res = z + w
+print(res)
+
+s = "Hello World!"
+print(s[-2])
+print(s[5:])
+
+x = -2
+y = abs(x)
+t = type(y)
+print(t)
+
+print(type(abs(x)))
+
+s = input("Saisir votre age: ")
+i = int(s)
+i = i + 1
+i += 1
+
+i /= 3
+
+print(f"Le résultat du calcul est {i:.2f}")
---
+++ tp1.py
@@ -0,0 +1,5 @@
+# Créer la variable current_year = 2024
+# Saisir votre année de naissance
+# Afficher votre age en fin d'année
+# Afficher votre age dans un an
+
