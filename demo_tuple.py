a6431e1390c82736853a6820b51e6a4c258e7e32
cyrilvincent <contact@cyrilvincent.com>
2024-10-16 10:51:01 +0200

    CEA

--- demo_tuple.py
+++ demo_tuple.py
@@ -1,15 +1,27 @@
-a = (1, 2)
-print(a[0], a[1])
-
-def toto() -> tuple[float, int]:
-    return 1.0, 2
-
-a, b = toto()
-print(a, b)
-
-l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# a = (1, 2)
+# print(a[0], a[1])
+#
+# def toto() -> tuple[float, int]:
+#     return 1.0, 2
+#
+# a, b = toto()
+# print(a, b)
+#
+# l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 def min_max_avg(l: list[int]) -> tuple[int, int, float]:
-    pass
+    total = 0
+    min = l[0]
+    max = l[0]
+    for value in l:
+        total += value
+        if value < min:
+            min = value
+        if value > max:
+            max = value
+    return min, max, total / len(l)
 
-# tester
+if __name__ == '__main__':
+    l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    min, max, avg = min_max_avg(l)
+    print(min, max, avg)
