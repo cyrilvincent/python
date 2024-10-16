5ad50dfa1b895cd748e7a884e1ba423186abb2ab
cyrilvincent <contact@cyrilvincent.com>
2024-10-16 14:44:14 +0200

    CEA

--- tp_heart.py
+++ tp_heart.py
@@ -1,3 +1,25 @@
 # Ouvrir le fichier data/heart/data_cleaned_up.csv
 # Compter combien de patient ont num==0 et num==1
 # Calculer le ratio sex/num => pour num=1 quel est le ratio sex=0 et sex=1
+
+import csv
+
+with open("data/heart/data_cleaned_up.csv") as f:
+    reader = csv.DictReader(f)
+    nb_male_ok = 0
+    nb_male_ko = 0
+    nb_female_ok = 0
+    nb_female_ko = 0
+    for row in reader:
+        sex = int(row["sex"])
+        num = int(row["num"])
+        if sex == 0 and num == 0:
+            nb_female_ok += 1
+        elif sex == 1 and num == 0:
+            nb_male_ok += 1
+        elif sex == 0 and num == 1:
+            nb_female_ko += 1
+        else:
+            nb_male_ko += 1
+    print(nb_female_ok, nb_male_ok, nb_female_ko, nb_male_ko)
+    print(nb_male_ok / nb_female_ok, nb_male_ko / nb_female_ko)
