7afaf43f5208e2e1b63edfe480011d484fbb8dcc
cyrilvincent <contact@cyrilvincent.com>
2024-12-02 10:46:34 +0100

    CEA

---
+++ tp1.py
@@ -0,0 +1,7 @@
+# Créer la variable a = 1 et b = 2
+# Permuter a et b
+# Saisir un nombre
+# Afficher le nombre de chiffre, 50 => 2
+# * par 2 le nombre et l'afficher
+# Afficher 0 si le nombre est pair et 1 sinon
+
