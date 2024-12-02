759201de8d525ae22936b2a75f6ef85c5c7b6c3b
cyrilvincent <contact@cyrilvincent.com>
2024-12-02 14:40:08 +0100

    CEA

---
+++ tp_boucle.py
@@ -0,0 +1,6 @@
+# Faire une boucle de 0 à 10
+# Faire une boucle de 10 à 0 inclus avec un pas de -2
+# Multiplier entre eux tous les nombres de 1 à 10 => factorielle
+# Afficher toutes la table des multiplications de 1 à 10 => 1*1=1, 1*2=2, .... 5*6=30, 10*10=100
+# Pas facile : afficher la suite de fibonacci jusqu'à 15 => f(15) = 610
+# Bonus : Afficher si n est premier sauf s'il possède un diviseur entre 2 et n-1
