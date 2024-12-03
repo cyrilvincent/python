c7a589a1f8d0b665234ef7543de577f3bdbf9da4
cyrilvincent <contact@cyrilvincent.com>
2024-12-03 15:29:41 +0100

    CEA

--- demo_tuple.py
+++ demo_tuple.py
@@ -7,3 +7,17 @@ print(x ,y, z)
 # def min_max_average(l): 1 seule iteration sum/len
 # return tuple des 3 float
 # test
+def min_max_average(l):
+    min = l[0]
+    max = l[0]
+    sum = 0
+    for value in l:
+        sum += value
+        if value < min:
+            min = value
+        elif value > max:
+            max = value
+    return min, max, sum / len(l)
+
+if __name__ == '__main__':
+    print(min_max_average(range(100)))
