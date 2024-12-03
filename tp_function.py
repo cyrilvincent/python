d0e39f53aa577aabd110f7b88fc75b38bfffb3fb
cyrilvincent <contact@cyrilvincent.com>
2024-12-03 08:58:35 +0100

    CEA

---
+++ tp_function.py
@@ -0,0 +1,8 @@
+# Créer la fonction add(a,b)
+# Créer la fonction is_even(n) => retourne True si n est pair False sinon
+# Créer la fonction sums(n) = somme de 0 à n inclus 5=>1+2+3+4+5
+# Créer la fonction factorielle(n) => n! pour n > 0
+# Créer la fonction fibo(n) => fibo(10) => 55
+# Créer la fonction is_prime(n) => retourne True si n est premier False sinon
+# Tester
+
