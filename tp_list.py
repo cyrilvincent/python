d0375df591710479d12580e6547932dfab531ab4
cyrilvincent <contact@cyrilvincent.com>
2024-12-03 13:31:47 +0100

    CEA

---
+++ tp_list.py
@@ -0,0 +1,8 @@
+# Créer "en dur" l = [1,2,5,99,10,-1,0,99,88,41] une liste avec 10 entiers
+# Afficher le premier et le dernier élément de la liste
+# Effacer le premier 99
+# Ajouter en fin de liste 999
+# Faire la fonction sum(l) -> sum([1,2,3]) => 6
+# Faire la fonction max(l) max([1,2,3]) => 3
+# + difficile filter_even([1,2,3,4]) => [2,4] il faut créer une nouvelle liste vide
+# Bonus filter_prime([1,2,3,4,5,6,7,8,9]) => [2,3,5,7]
