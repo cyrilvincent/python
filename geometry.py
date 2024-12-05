05e7568582b8f6eb18d02496e4bdb86ee098e048
cyrilvincent <contact@cyrilvincent.com>
2024-12-05 14:09:00 +0100

    CEA

--- geometry.py
+++ geometry.py
@@ -1,10 +1,25 @@
 import datetime
 
+class Coord:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move_rel(self, x, y):
+        self.x += x
+        self.y += y
+
 class Rectangle:
 
-    def __init__(self, length, width):
+    def __init__(self, length, width, coord):
         self.length = length
         self.width = width
+        self.coord = coord
 
     def area(self):
         return self.length * self.width
@@ -21,9 +36,13 @@ class Rectangle:
 
 
 if __name__ == '__main__':
-    r1 = Rectangle(3,2)
+    c1 = Coord(-2,4)
+    c2 = Coord(1,0)
+    r1 = Rectangle(3,2,c1)
+    r1.coord.move_rel(3,4)
+    print(r1.coord.x, r1.coord.y)
     print(r1.length, r1.width)
     print(r1.area(), r1.perimeter())
-    r2 = Rectangle(4,5)
+    r2 = Rectangle(4,5, Coord(6,1))
     print(r2.area(), r2.perimeter())
 
--- tp_media.py
+++ tp_media.py
@@ -1,4 +1,24 @@
+# Class Publisher
+# name, mail, phone
+# Lier Book à Publisher et tester
+# Class Author
+# first_name, last_name, phone
+# Lier Book à Authors et tester
+
 class Book:
 
-    # QUOI __init + self.price
-    # net_price = price * 1.055
+    def __init__(self, title, price, year, lang, nb_page=0):
+        self.title = title
+        self.price = price
+        self.year = year
+        self.nb_page = nb_page
+        self.lang = lang
+
+    def net_price(self):
+        return self.price * 1.055
+
+
+if __name__ == '__main__':
+    b1 = Book("Les misérables", 10, 1862, "fr-FR")
+    print(f"Book {b1.title} {b1.net_price():.2f}€")
+
