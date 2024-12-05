7a31161d9b32826f9d95de6d176b0e36ee72c01f
cyrilvincent <contact@cyrilvincent.com>
2024-12-05 12:02:09 +0100

    CEA

---
+++ geometry.py
@@ -0,0 +1,20 @@
+class Rectangle:
+
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+
+if __name__ == '__main__':
+    r1 = Rectangle(3,2)
+    print(r1.length, r1.width)
+    print(r1.area(), r1.perimeter())
+    r2 = Rectangle(4,5)
+    print(r2.area(), r2.perimeter())
+
---
+++ tp_media.py
@@ -0,0 +1,4 @@
+class Book:
+
+    # QUOI __init + self.price
+    # net_price = price * 1.055
