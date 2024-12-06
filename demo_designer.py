5cbbfe6c5694e9398d23ac7ee46c8b682c07aa8a
cyrilvincent <contact@cyrilvincent.com>
2024-12-06 11:45:35 +0100

    CEA

---
+++ demo_designer.py
@@ -0,0 +1,22 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QVBoxLayout, QLineEdit, QLabel
+from PyQt6 import uic
+import sys
+import tp_function
+
+class Ui(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("ui/MainWindow.ui", self)
+        self.pushButton.clicked.connect(self.button_clicked)
+
+    def button_clicked(self):  # Slot
+        value = self.lineEdit.text()
+        value = int(value)
+        result = tp_function.is_prime(value)
+        self.label.setText(f"Le nombre {value} est premier: {result}")
+
+app = QApplication(sys.argv)
+window = Ui()
+window.show()
+app.exec()
