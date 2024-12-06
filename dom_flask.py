1c50208a1e2ff2e280718b4a6e646ed97ab8ef8a
cyrilvincent <contact@cyrilvincent.com>
2024-12-06 10:04:49 +0100

    CEA

--- dom_flask.py
+++ dom_flask.py
@@ -22,23 +22,31 @@ def is_prime(x):
     else:
         return f"Le nombre {x} n'est pas premier"
 
-@app.route("/house")
-def house():
+@app.route("/house/<int:surface>/<type>")
+def house(surface, type):
     dataframe = pd.read_csv("data/house/house.csv")
-    html = dataframe.to_html()
-    return html
+    dataframe = dataframe[dataframe.surface < surface]
+    if type == "html":
+        return dataframe.to_html(index=False)
+    elif type == "csv":
+        return dataframe.to_csv(index=False)
+    elif type == "excel":
+        return dataframe.to_excel(index=False)
+    else:
+        return dataframe.to_json(index=False)
+
+@app.route("/house/describe")
+def describe():
+    dataframe = pd.read_csv("data/house/house.csv")
+    describe = dataframe.describe()
+    return describe.to_html(index=False)
+
 # TP
 # Reprendre mon code
 # Ajouter une variable dans /house/200 qui filtre les houses par surface < 200
 # Créer la route /house/describe qui affiche le describe(), describe retourne un dataframe, to_html
 
-@app.route("/xsinx")
-def xsinx():
-    x = np.arange(0, 10, 0.1)
-    y = x * np.sin(x)
-    plt.plot(x, y)
-    plt.savefig("data/fig.png")
-    return f"<img src='/data/fig.png'>"
+
 
 
 
