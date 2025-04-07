09d35eb24004f5d7b521f13ea337404fbc3398a2
Cyril Vincent <contact@cyrilvincent.com>
2025-04-07 13:40:46 +0200

    CNRS

---
+++ tp1.py
@@ -0,0 +1,6 @@
+# Créer la variable current_year= = 2025
+# Saisir votre année de naissance
+# Stocker dans une variable votre age + 1 et l'afficher
+# Saisir un mot dans une variable
+# Afficher les 3 1er caractères du mot
+# Afficher tous les caractères sauf le 1er et le dernier : python => ytho
