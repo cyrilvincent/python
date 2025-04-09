4054604bb0e626bbdfbe3a8675f1be29b0b67520
Cyril Vincent <contact@cyrilvincent.com>
2025-04-09 10:42:21 +0200

    CNRS

--- tp_house.py
+++ tp_house.py
@@ -10,3 +10,9 @@ print(loyers)
 # loyer_per_m2 + min, max, mean
 # surfaces_sup_200 + len
 
+print(np.min(loyers), np.max(loyers), np.mean(loyers), len(loyers))
+print(np.min(surfaces), np.max(surfaces), np.mean(surfaces), len(surfaces))
+
+loyer_per_m2 = loyers / surfaces
+print(np.min(loyer_per_m2), np.max(loyer_per_m2), np.mean(loyer_per_m2), len(loyer_per_m2))
+
