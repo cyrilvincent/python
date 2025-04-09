3dce8b777c49224d0d6f369633e3726ec1661bbf
Cyril Vincent <contact@cyrilvincent.com>
2025-04-09 14:47:32 +0200

    CNRS

--- demo_pandas.py
+++ demo_pandas.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # dataframe = pd.read_csv("data/house/house.csv")
-dataframe = pd.read_excel("data/house/house.xlsx") # pip install xlrd
+dataframe = pd.read_excel("data/house/house.xlsx") # pip install openpyxl
 dataframe["loyers_per_m2"] = dataframe.loyer / dataframe["surface"]
 print(dataframe.describe())
 dataframe.to_html("data/house/house.html")
