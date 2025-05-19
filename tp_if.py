303cdf681befeeab37e673b95b72cb17c7b6449b
cyrilvincent <contact@cyrilvincent.com>
2025-05-19 13:42:33 +0200

    CEA

--- tp_if.py
+++ tp_if.py
@@ -8,3 +8,24 @@
 
 # nb_quarter = ??
 # Rappel : 64 ans ET 172 quarters
+
+age = 65
+nb_quarter = 168
+if age<1:
+    print("BB")
+elif age < 12:
+    print("Enfant")
+elif age < 18:
+    print("Ado")
+elif age < 64:
+    print("Actif")
+elif age < 67:
+    if nb_quarter >= 172:
+        print("Retraité à taux plein")
+    else:
+        nb_future_quarter = 172 - nb_quarter
+        print(f"Il vous reste {nb_future_quarter} trimestre(s) à travailler")
+elif age < 100:
+    print("Retraité")
+else:
+    print("Centenaire")
