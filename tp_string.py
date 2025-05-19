2f51308f23e664dac1b4bb09268af88782beda91
cyrilvincent <contact@cyrilvincent.com>
2025-05-19 11:20:33 +0200

    CEA

---
+++ tp_string.py
@@ -0,0 +1,5 @@
+# Saisir un mot
+# Afficher le nb de lettre du mot
+# Afficher le 3ème caractère (index 2)
+# Afficher les 3 1er catactères
+# Afficher tout sauf le 1er det dernier caractère, par exemple python => ytho
