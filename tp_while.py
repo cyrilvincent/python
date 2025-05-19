2cc8fa58207356490dbd1763cfe9fa1e65d60d80
cyrilvincent <contact@cyrilvincent.com>
2025-05-19 13:58:21 +0200

    CEA

---
+++ tp_while.py
@@ -0,0 +1,4 @@
+# Afficher les nombres multiples de 3 entre 0 et 100
+# Soit n = entier >= 1, afficher n! 5! = 1*2*3*4*5 = 120
+# + difficile, soit word = "radar" afficher si c'est un palindrome
+# Bonus : soit n un entier >= 2, est-il premier?
