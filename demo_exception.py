7aaa6cb2899f29e96d9bf0ca8c37035c44173eb0
cyrilvincent <contact@cyrilvincent.com>
2025-05-20 10:50:45 +0200

    CEA

--- demo_exception.py
+++ demo_exception.py
@@ -1,7 +1,16 @@
-age = input("Age: ")
-age = int(age)
-print(age)
+
 
 # Resaisir l'age indefiniment tant que ce n'est pas un entier
 # Tips: while True + break
 # Idem pour les ages < 0 : raise
+
+while True:
+    try:
+        age = input("Age: ")
+        age = int(age)
+        if age < 0:
+            raise ValueError("Age négatif")
+        print(age)
+        break
+    except ValueError as error:
+        print(f"Erreur sur l'age : {error}")
