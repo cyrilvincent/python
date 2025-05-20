312d9fe7484b4ac653b309604777a49d35bafb42
cyrilvincent <contact@cyrilvincent.com>
2025-05-20 13:35:02 +0200

    CEA

--- my_module.py
+++ my_module.py
@@ -1,3 +1,5 @@
 # sum
 # tester dans le if __name__=='__main__'
 # max
+# filter_even(l: list[int]) -> list[int] : [1,2,3,4] => [2,4]
+# filter_prime(l: list[int]) -> list[int] : [1,2,3,4,5,6,7] => [2,3,5,7]
