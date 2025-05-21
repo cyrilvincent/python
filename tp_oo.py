cb6992e2ceb0c9a7558ca6ba19cba38712ab6d90
cyrilvincent <contact@cyrilvincent.com>
2025-05-21 14:26:27 +0200

    CEA

--- tp_oo.py
+++ tp_oo.py
@@ -1,20 +1,5 @@
 import datetime
 
-class Instrument:
-
-    def __init__(self, name: str, ip: str, state: bool = True, measure_types: list[str] = [], date_calibration: datetime.datetime = datetime.datetime.now()):
-        self.name = name
-        self.ip = ip
-        self.state = state
-        self.measure_types = measure_types
-        self.date_calibration = date_calibration
-
-    def calibrate(self):
-        self.date_calibration = datetime.datetime.now()
-
-    def measure(self, frequency_range: tuple[float, float], nb_point: int=1):
-        pass
-
 
 class Measure:
 
@@ -24,7 +9,33 @@ class Measure:
         self.values = values
 
 
+class Instrument:
+
+    def __init__(self, name: str,
+                 ip: str,
+                 state: bool = True,
+                 measure_types: list[str] = [],
+                 date_calibration: datetime.datetime = datetime.datetime.now(),
+                 measures: list[Measure] = []):
+        self.name = name
+        self.ip = ip
+        self.state = state
+        self.measure_types = measure_types
+        self.date_calibration = date_calibration
+        self.measures = measures
+
+    def calibrate(self):
+        self.date_calibration = datetime.datetime.now()
+
+    def measure(self, frequency_range: tuple[float, float], nb_point: int=1):
+        for i in range(nb_point):
+            self.measures.append(Measure(i,"ms",[0,1,2]))
+
+
 if __name__ == '__main__':
     m1 = Measure(0,"ms",[1,2,3])
     assert m1.t == 0
+    i1 = Instrument("Voltmeter", "192.168.0.1")
+    i1.measure((0,0.1), 10)
+    assert len(i1.measures) == 10
 
