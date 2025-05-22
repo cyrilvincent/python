3d5fca4f80283eda3f378e27602e8ab35579762e
cyrilvincent <contact@cyrilvincent.com>
2025-05-22 14:04:46 +0200

    CEA

--- demo_qtdesigner_pyuic.py
+++ demo_qtdesigner_pyuic.py
@@ -4,12 +4,19 @@ from PyQt6 import uic
 from PyQt6.QtWidgets import QMainWindow, QPushButton, QApplication, QLayout, QLineEdit, QLabel, QVBoxLayout, QWidget
 import tp_function
 import mainwindow # python -m PyQt6.uic.pyuic ui\MainWindow.ui -o mainwindow.py
+import mywidget # python -m PyQt6.uic.pyuic ui\MyWidget.ui -o mywidget.py
+
+class MyWidget(QWidget, mywidget.Ui_Form):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
 class MainWindow(QMainWindow, mainwindow.Ui_MainWindow):
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.mywidget = MyWidget()
         self.pushButton.clicked.connect(self.button_clicked)
         self.errorLabel.setVisible(False)
         self.actionPrime.triggered.connect(self.button_clicked)
@@ -21,9 +28,13 @@ class MainWindow(QMainWindow, mainwindow.Ui_MainWindow):
             result = tp_function.is_prime(nb)
             self.label_2.setText(str(result))
             self.errorLabel.setVisible(False)
+            self.mywidget.show()
         except:
             self.errorLabel.setVisible(True)
             self.label_2.setText("")
+
+
+
 
 
 app = QApplication(sys.argv)
