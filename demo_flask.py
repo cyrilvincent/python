c3b58107afd20bc880019750d11d1b214348fcd5
cyrilvincent <contact@cyrilvincent.com>
2025-05-22 08:47:32 +0200

    CEA

--- demo_flask.py
+++ demo_flask.py
@@ -1,5 +1,6 @@
 import flask
 import tp_function
+import demo_dico
 
 app = flask.Flask(__name__)
 
@@ -10,6 +11,15 @@ def root():
 @app.route("/prime/<int:nb>")
 def prime(nb: int):
     return str(tp_function.is_prime(nb))
+
+@app.route("/config")
+def config():
+    return demo_dico.config
+
+@app.route("/instrument/<int:id>")
+def instrument(id: int):
+    return demo_dico.config
+
 
 app.run(host="0.0.0.0")
 
