00255f0a41431bc9d7f6fdbe5397fd42f90737c7
cyrilvincent <contact@cyrilvincent.com>
2025-05-22 15:26:39 +0200

    CEA

--- demo_numpy.py
+++ demo_numpy.py
@@ -1,3 +1,19 @@
-import numpy
+import numpy as np
 
-print(numpy.__version__)
+print(np.__version__)
+
+a1 = np.array([1,2,3,4])
+print(a1)
+np.random.seed(42)
+rnd1 = np.random.rand(4)
+print(rnd1)
+print(a1 * 2)
+a2 = np.array([5,6,7,8])
+print(a1 * a2)
+a3 = np.array([1,2,3])
+# print(a2 * a3)
+for i in a2:
+    print(i)
+print(np.sin(a2))
+print(np.dot(a2, a1))
+print(np.sin(0), np.sin(np.array([0,1])), np.sin(np.array([[1,2],[3,4]])))
