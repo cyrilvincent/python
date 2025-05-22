95120bb85ff46004d59f6c14cf5fac98f1c81be7
cyrilvincent <contact@cyrilvincent.com>
2025-05-22 10:48:12 +0200

    CEA

--- demo_qt.py
+++ demo_qt.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt6.QtWidgets import QMainWindow, QPushButton, QApplication
-
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QApplication, QLayout, QLineEdit, QLabel, QVBoxLayout, QWidget
+import tp_function
 
 class MainWindow(QMainWindow):
 
@@ -8,13 +8,28 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Formation Python")
         self.button = QPushButton("OK")
-
-        self.setCentralWidget(self.button)
+        self.lineEdit = QLineEdit()
+        self.label = QLabel()
+        self.layout = QVBoxLayout()
+        self.errorLabel = QLabel("Erreur")
+        self.errorLabel.setVisible(False)
+        self.layout.addWidget(self.lineEdit)
+        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.errorLabel)
+        container = QWidget()
+        container.setLayout(self.layout)
+        self.setCentralWidget(container)
         self.button.clicked.connect(self.button_clicked)
 
     def button_clicked(self):
-        self.button.setText("Clicked")
-        self.setWindowTitle("Clicked")
+        try:
+            nb = int(self.lineEdit.text())
+            result = tp_function.is_prime(nb)
+            self.label.setText(str(result))
+            self.errorLabel.setVisible(False)
+        except:
+            self.errorLabel.setVisible(True)
 
 
 app = QApplication(sys.argv)
