f3fd7967167023f4c4b618ff838d8edc8d1b4a1f
cyrilvincent <contact@cyrilvincent.com>
2025-06-16 15:04:05 +0200

    CEA

--- tp_for.py
+++ tp_for.py
@@ -1,1 +1,26 @@
 # Porte tp_while en for
+
+n = 5
+facto = 1
+for i in range(2, n+1):
+    facto *= i
+print(f"{n}!={facto}")
+
+n = 10
+fibo = 1
+fibo2 = 0
+for i in range(1, n):
+    temp = fibo
+    fibo = fibo + fibo2
+    fibo2 = temp
+print(f"F({n})={fibo}")
+
+n = 1224
+is_prime = True
+for div in range(2, n):
+    if n % div == 0:
+        print("Non premier")
+        is_prime = False
+        break
+if is_prime:
+    print("Premier")
