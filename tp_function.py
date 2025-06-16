5f43c063117fdceb9b9348896ea42b0449ab5d2f
cyrilvincent <contact@cyrilvincent.com>
2025-06-16 16:12:33 +0200

    CEA

--- tp_function.py
+++ tp_function.py
@@ -1,3 +1,31 @@
 # Créer les fonctions facto, fibo, is_prime
 # Typer : signature
 # Tester
+
+def factorielle(n: int) -> int:
+    facto = 1
+    for i in range(2, n + 1):
+        facto *= i
+    return facto
+
+def fibonacci(n: int) -> int:
+    fibo = 1
+    fibo2 = 0
+    for i in range(1, n):
+        temp = fibo
+        fibo = fibo + fibo2
+        fibo2 = temp
+    return fibo
+
+def is_prime(n: int) -> bool:
+    is_prime = True
+    for div in range(2, n):
+        if n % div == 0:
+            is_prime = False
+            break
+    return is_prime
+
+print(factorielle(5))
+print(fibonacci(10))
+print(is_prime(7))
+print(is_prime(8))
