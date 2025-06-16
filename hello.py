3b216e671f91202c3e6299548882baa7569987a2
cyrilvincent <contact@cyrilvincent.com>
2025-06-16 11:00:33 +0200

    CEA

--- hello.py
+++ hello.py
@@ -2,3 +2,36 @@ print("Hello World!")
 a = 1
 a = a + 1
 print(a)
+b = 3.14
+c = a + b
+print(f"{c:.2f}")
+print(type(c))
+d = "hello"
+print(type(d))
+# print(a + d)
+
+# a = 1
+# b = "2"
+# print(a + b)
+
+a = 1
+b = 2
+print(a + b)
+
+a = "1"
+b = "2"
+print(a + b)
+
+a = "tot\n\t\"o"
+print(len(a))
+print(a)
+n = """fghkdgflhkjfghkjh
+fdhgrtfhfghgf
+dfghfghfgdhfdg"""
+print(n)
+
+x = input("x: ")
+y = float(x)
+print(y + 1)
+
+print(f"Vous avez saisi la valeur {y:.2f}")
---
+++ tp_string.py
@@ -0,0 +1,3 @@
+# Créer la variable year = 2025
+# Saisir vote année de naissance
+# Afficher votre age en fin d'année
