d4b0c4bda74dc83a6e81853b15baca45b3080d0b
cyrilvincent <contact@cyrilvincent.com>
2025-06-16 11:18:06 +0200

    CEA

--- tp_string.py
+++ tp_string.py
@@ -1,3 +1,9 @@
 # Créer la variable year = 2025
 # Saisir vote année de naissance
 # Afficher votre age en fin d'année
+
+year = 2025
+birth_string = input("Année de naissance: ")
+birth_year = int(birth_string)
+age = year - birth_year
+print(f"Vous avez {age} ans")
