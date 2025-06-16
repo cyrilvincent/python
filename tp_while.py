2e670f12246d9f6a73bf7340fda20526c2731f02
cyrilvincent <contact@cyrilvincent.com>
2025-06-16 14:32:26 +0200

    CEA

--- tp_while.py
+++ tp_while.py
@@ -4,3 +4,35 @@
 # Bonus: Fibonacci (3 variables)
 # Bonus difficile : Soit n=7 est ce que n est premier?  divisible uniquement par 1 et lui-même
 # Tout nombre > 1 est premier sauf s'il possède un diviseur entre 2 et n-1
+
+n = 5
+i = 2
+facto = 1
+while i <= n:
+    facto *= i
+    i += 1
+print(f"{n}!={facto}")
+
+n = 10
+i = 1
+fibo = 1
+fibo2 = 0
+while i < n:
+    temp = fibo
+    fibo = fibo + fibo2
+    fibo2 = temp
+    i += 1
+print(f"F({n})={fibo}")
+
+n = 1223
+div = 2
+if n < 2:
+    print("Non premier")
+else:
+    while div < n:
+        if n % div == 0:
+            print("Non premier")
+            break
+        div += 1
+    if div == n:
+        print("Premier")
