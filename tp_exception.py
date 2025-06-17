309afe5af2064204dddb97a5402e09b08fc4c86f
cyrilvincent <contact@cyrilvincent.com>
2025-06-17 10:17:29 +0200

    CEA

--- tp_exception.py
+++ tp_exception.py
@@ -2,6 +2,8 @@ while True:
     try:
         age_string = input("Age: ")
         age = int(age_string)
+        if age < 0 or age > 120:
+            raise ValueError("Age incohérent")
         print(age)
         break
     except ValueError as ex:
