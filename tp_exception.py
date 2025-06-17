100197968a2dabc78a85751bd91260bb40f85b17
cyrilvincent <contact@cyrilvincent.com>
2025-06-17 10:14:50 +0200

    CEA

--- tp_exception.py
+++ tp_exception.py
@@ -1,6 +1,8 @@
-try:
-    age_string = input("Age: ")
-    age = int(age_string)
-    print(age)
-except ValueError as ex:
-    print(f"Erreur: {ex}")
+while True:
+    try:
+        age_string = input("Age: ")
+        age = int(age_string)
+        print(age)
+        break
+    except ValueError as ex:
+        print(f"Erreur: {ex}")
