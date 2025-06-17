796b4a50b72a348c4a3826ac14bce78d87a3ad34
cyrilvincent <contact@cyrilvincent.com>
2025-06-17 14:08:49 +0200

    CEA

---
+++ demo_tuple.py
@@ -0,0 +1,10 @@
+def my_function() -> tuple[float, float, int, str]:
+    # N'importe quoi
+    return 1.0, 2.0, 3, "toto"
+
+
+a,b,c,d = my_function()
+print(a,b,c,d)
+
+
+
---
+++ tp_tuple.py
@@ -0,0 +1,3 @@
+def avg_min_max(l: list[int]) -> tuple[float, int, int]:
+    return 0,0,0
+
