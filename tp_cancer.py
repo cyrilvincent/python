6d1122253507b153345c3ba10c5fa0f3afd77208
cyrilvincent <contact@cyrilvincent.com>
2025-06-18 14:23:11 +0200

    CEA

--- tp_cancer.py
+++ tp_cancer.py
@@ -1,11 +1,41 @@
 # Charger data/cancer/data.csv
 # Créer les listes radius_worsts, area_worsts, ids, diagnosis
 # Afficher la moyenne des listes
+import csv
 
-diagnosis = []
-radius_worsts = []
-for index in range(len(diagnosis)):
-    print(diagnosis[index])
-    print(radius_worsts[index])
+class CancerService:
 
-# Bonus moyenne des radius pour diagnosis==0 et diagnosis==1
+    def __init__(self):
+        self.ids = []
+        self.diagnosis = []
+        self.radius = []
+        self.areas = []
+        self.radius0 = []
+        self.radius1 = []
+
+    def load(self, path):
+        with open(path) as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                self.ids.append(int(row["id"]))
+                diagnosis = int(row["diagnosis"])
+                self.diagnosis.append(diagnosis)
+                self.radius.append(float(row["radius_worst"]))
+                self.areas.append(float(row["area_worst"]))
+                if diagnosis == 0:
+                    self.radius0.append(float(row["radius_worst"]))
+                else:
+                    self.radius1.append(float(row["radius_worst"]))
+
+    def avg(self, l: list[float]) -> float:
+        return sum(l) / len(l)
+
+
+
+if __name__ == '__main__':
+    c = CancerService()
+    c.load("data/cancer/data.csv")
+    print(c.ids)
+    print(c.avg(c.radius))
+    print(c.avg(c.radius0))
+    print(c.avg(c.radius1))
