a3bbf90bae0aa203d30ad54ddabbde1757312d15
cyrilvincent <contact@cyrilvincent.com>
2025-06-18 14:32:46 +0200

    CEA

--- tp_cancer.py
+++ tp_cancer.py
@@ -2,6 +2,7 @@
 # Créer les listes radius_worsts, area_worsts, ids, diagnosis
 # Afficher la moyenne des listes
 import csv
+import pickle
 
 class CancerService:
 
@@ -30,12 +31,24 @@ class CancerService:
     def avg(self, l: list[float]) -> float:
         return sum(l) / len(l)
 
+    def save(self):
+        with open("data/cancer/data.pkl", "wb") as f:
+            pickle.dump(self, f)
+
+    def load_pickle(self):
+        with open("data/cancer/data.pkl", "rb") as f:
+            self = pickle.load(f)
+
 
 
 if __name__ == '__main__':
     c = CancerService()
     c.load("data/cancer/data.csv")
+    c.save()
+    c.load_pickle()
     print(c.ids)
     print(c.avg(c.radius))
     print(c.avg(c.radius0))
     print(c.avg(c.radius1))
+
+
