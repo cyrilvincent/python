1af3d721a38368a83dcaa03cce45a01f11013896
cyrilvincent <contact@cyrilvincent.com>
2025-06-18 15:32:42 +0200

    CEA

--- demo_numpy.py
+++ demo_numpy.py
@@ -1,7 +1,8 @@
 import numpy as np
 import tp_cancer
 
-v1 = np.array([1,2,3,4])
+v1 = np.array([1,2,3,4], dtype=np.uint8)
+print(v1 + 1)
 print(v1)
 v2 = np.array([5,6,7,8])
 print(v1 * v2)
@@ -13,3 +14,10 @@ s.load("data/cancer/data.csv")
 
 print(np.mean(s.radius0), np.std(s.radius0), np.median(s.radius0))
 print(np.mean(s.radius1), np.std(s.radius1), np.median(s.radius1))
+
+print(np.arange(0,100,10))
+print(np.linspace(0,100,11))
+
+np.random.seed(42)
+print(np.random.randint(0,100))
+print(np.random.rand(10) * 100)
