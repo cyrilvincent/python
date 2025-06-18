bcd63008aa7e94408e59565b6f6f5cf5ae3d2541
cyrilvincent <contact@cyrilvincent.com>
2025-06-18 10:50:56 +0200

    CEA

--- demo_oo.py
+++ demo_oo.py
@@ -1,8 +1,24 @@
+class Point:
+
+    def __init__(self,x, y):
+        self.x = x
+        self.y = y
+
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move_relative(self, x, y):
+        self.x += x
+        self.y += y
+
+
 class Rectangle:
 
-    def __init__(self, width, length):
+    def __init__(self, width, length, coord: Point = Point(0,0)):
         self.width = width
         self.length = length
+        self.coord = coord
 
     def area(self):
         return self.width * self.length
@@ -14,8 +30,10 @@ if __name__ == '__main__':
     r1 = Rectangle(3,2)
     assert 6 == r1.area()
     assert 10 == r1.perimeter()
-    r2 = Rectangle(3,3)
+    r2 = Rectangle(3,3, Point(4,5))
     rectangles: list[Rectangle] = [r1, r2]
     for r in rectangles:
         print(r.area())
     print([r.area() + r.perimeter() for r in rectangles])
+    assert 4 == r2.coord.x
+    r2.coord.move(0,0)
--- tp_oo.py
+++ tp_oo.py
@@ -1,17 +1,37 @@
+import datetime
+
+class Personne:
+
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+
+class Transaction:
+
+    def __init__(self, montant: float, date: datetime.datetime=datetime.datetime.now()):
+        self.montant = montant
+        self.date = date
+
+
 class BankAccount:
 
     count = 1
 
-    def __init__(self, banque: str, devise="EUR"):
+    def __init__(self, banque: str, titulaire: Personne, devise="EUR"):
         self.solde = 0
         self.id = BankAccount.count
         BankAccount.count += 1
         self.devise = devise
         self.banque = banque
+        self.titulaire = titulaire
+        self.transactions: list[Transaction] = []
 
     def crediter(self, montant: float):
         if montant > 0:
             self.solde += montant
+            transac = Transaction(montant)
+            self.transactions.append(transac)
+
         else:
             raise ValueError("Montant < 0")
 
@@ -19,6 +39,8 @@ class BankAccount:
         if montant <= self.solde:
             if montant > 0:
                 self.solde -= montant
+                transac = Transaction(-montant)
+                self.transactions.append(transac)
                 return montant
             else:
                 raise ValueError("Montant < 0")
@@ -26,18 +48,21 @@ class BankAccount:
             raise ValueError("Découvert non autorisé")
 
 if __name__ == '__main__':
-    c1 = BankAccount("BP")
+    p1 = Personne("Cyril", "Vincent")
+    c1 = BankAccount("BP", p1)
     c1.crediter(100)
     argent = c1.debiter(60)
     assert 60 == argent
     assert 40 == c1.solde
     assert 1 == c1.id
+    assert "Cyril" == c1.titulaire.fname
+    assert 2 == len(c1.transactions)
     try:
         c1.debiter(41)
         assert False
     except ValueError:
         assert True
-    c2 = BankAccount("CM")
+    c2 = BankAccount("CM", p1)
     c2.crediter(200)
     assert c1.solde != c2.solde
     assert 2 == c2.id
