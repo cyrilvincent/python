fe065b9b6ba261ba8ffbfe3b2c9ebdbf5e90a28a
cyrilvincent <contact@cyrilvincent.com>
2025-06-19 11:53:30 +0200

    CEA

--- tp_matplotlib.py
+++ tp_matplotlib.py
@@ -2,3 +2,10 @@
 # Ajouter title
 # Ajouter des label et legend
 # Ajouter des couleurs
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataframe = pd.read_csv("data/house/house.csv")
+plt.scatter(dataframe.surface, dataframe.loyer)
+plt.show()
