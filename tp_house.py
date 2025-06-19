8ab648bc8c12d20f09b4bc384973b11e9e991f38
cyrilvincent <contact@cyrilvincent.com>
2025-06-19 09:59:02 +0200

    CEA

--- tp_house.py
+++ tp_house.py
@@ -1,8 +1,23 @@
 import numpy as np
+import unittest
 
-print(np.load("data/house/house.npz"))
+data = np.load("data/house/house.npz")
+print(data)
+
+
+loyers = data["loyers"]
+surfaces = data["surfaces"]
+
+filter = surfaces < 100
+loyers_filter = loyers[filter]
+surfaces_filter = surfaces[filter]
+loyer_m2 = loyers_filter / surfaces_filter
+print(np.mean(loyer_m2), np.median(loyer_m2), np.std(loyer_m2))
+
+
+loyers_filter[:100:2]
 
 # Retrouver loyers & surfaces
 # Recalculer loyer_m2
-# Filtrer les loyer et surface < 100
+# Filtrer surface < 100
 # Faire les stats de base
