4a368c95e5abe55b12bd06e520e701b965ffe23f
cyrilvincent <contact@cyrilvincent.com>
2026-02-16 11:31:53 +0100

    Enwires

---
+++ demo_for.py
@@ -0,0 +1,9 @@
+# i = 0
+# while i < 10:
+#     print(i)
+#     i+=1 # <=> i=i+1
+
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
--- tp1.py
+++ tp1.py
@@ -5,3 +5,8 @@ year = input("Année de naissance: ")
 year_int = int(year)
 age = 2026 - year_int
 print(f"Vous avez {age} ans")
+
+if age < 18:
+    print("Mineur")
+else:
+    print("Majeur")
