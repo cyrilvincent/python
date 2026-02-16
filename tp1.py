e3a1d6feeea3a69347e6d3f1ab7515b1e6b997f1
cyrilvincent <contact@cyrilvincent.com>
2026-02-16 10:41:49 +0100

    Enwires

--- tp1.py
+++ tp1.py
@@ -1,2 +1,7 @@
 # Saisir son année de naissance
 # Afficher son age en fin d'année
+
+year = input("Année de naissance: ")
+year_int = int(year)
+age = 2026 - year_int
+print(f"Vous avez {age} ans")
