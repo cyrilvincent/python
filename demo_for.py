9b60c51331689b64599305b72c21d88744977371
Cyril Vincent <contact@cyrilvincent.com>
2026-02-23 08:12:33 +0100

    CEA

--- demo_for.py
+++
@@ -1,9 +0,0 @@
-# i = 0
-# while i < 10:
-#     print(i)
-#     i+=1 # <=> i=i+1
-
-for i in range(10):
-    if i == 5:
-        break
-    print(i)
--- demo_tuple.py
+++
@@ -1,6 +0,0 @@
-def my_function() -> tuple[float, float, str]:
-    return 2.14, 3.14, "red"
-
-x,y,color = my_function()
-print(x, y, color)
-
