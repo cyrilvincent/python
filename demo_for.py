861d7bbe049f0d0385bab0ecfbd5f3ebf8dc32f5
cyrilvincent <contact@cyrilvincent.com>
2026-02-23 14:05:21 +0100

    CEA

---
+++ demo_for.py
@@ -0,0 +1,19 @@
+for i in range(10):
+    print(i)
+
+i=2
+while i < 10:
+    print(i)
+    i+=3
+
+for x in range(10):
+    for y in range(10):
+        print(f"({x}+{y})={x+y}")
+
+n = 5
+facto= 1
+for x in range(2, n + 1):
+    facto = facto * x
+print(facto)
+
+
---
+++ tp_boucle.py
@@ -0,0 +1,7 @@
+# Afficher les nombre pairs < 100 (x%2==0)
+# Afficher la table des multiplications <= 10
+# Refaire la factorielle
+# Pour tous les nombres n < 100, si n est pair affiche le carré du nombre sinon afficher sa racine carrée ** 0.5
+# Calculer le nieme nombre de la suite de fibonacci (par exemple pour n=10 => 55)
+# Bonus : Dire si n est premier ou non, n > 0 et entier est premier ssi il possède 2 diviseurs 1 et lui même
+#   Tout nombre n > 0 est premier SAUF s'il possède un diviseur entre 2 et n-1
