d05dffa76a3da6de2251e8b12c86d84b2f8250bb
cyrilvincent <contact@cyrilvincent.com>
2026-02-23 11:28:45 +0100

    CEA

--- tp1.py
+++ tp1.py
@@ -3,7 +3,13 @@
 # Afficher votre age en fin d'année
 
 year = 2026
+toto = 2
+titi = 3
 birth = input("Année de naissance: ")
-birth_int = int(birth)
-age = year - birth_int
+birth = int(birth)
+age = year - birth
 print(f"Vous avez {age} ans")
+
+condition = (year > 2026 or titi==3) and toto == 2
+if condition:
+    print("Trop grand")
---
+++ tp_retraite.py
@@ -0,0 +1,7 @@
+nb_trimestre = 40
+year_birth = 1972
+# Si née après 1966
+# Si >= 172 trimestres ET 62 ans = taux plein
+# Si 67 ans = taux plein
+# Si < 172 trimestres > 62 ans
+# Age de départ à taux plein (172-nb_trimestre)/4+62
