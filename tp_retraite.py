8ad51f64457a4fdc72020ef80596dc12946c72eb
cyrilvincent <contact@cyrilvincent.com>
2026-02-23 12:06:16 +0100

    CEA

--- tp_retraite.py
+++ tp_retraite.py
@@ -1,7 +1,21 @@
-nb_trimestre = 40
+nb_trimestre = 122
 year_birth = 1972
+year = 2026
+age = year - year_birth
 # Si née après 1966
 # Si >= 172 trimestres ET 62 ans = taux plein
 # Si 67 ans = taux plein
-# Si < 172 trimestres > 62 ans
-# Age de départ à taux plein (172-nb_trimestre)/4+62
+# Si < 172 trimestres
+# Age de départ à taux plein (172-nb_trimestre)/4+age
+
+if age >= 67:
+    print("Taux plein")
+elif age < 62:
+    print("Faut encore travailler")
+if age < 67:
+    remaining = (172 - nb_trimestre) / 4
+    age_depart = age + remaining
+    if age_depart > 67:
+        age_depart = 67
+    print(f"Age de départ à la retraite: {age_depart}")
+
