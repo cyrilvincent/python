2ebcb4697a7aa950bd32b4a2efa22ff9fb7cb41b
cyrilvincent <contact@cyrilvincent.com>
2026-02-24 13:18:11 +0100

    CEA

--- demo_tuple.py
+++ demo_tuple.py
@@ -1,10 +1,16 @@
-def toto() -> tuple[float, float]:
-    return 1, 2
+def min_max_avg(l: list[float]) -> tuple[float, float, float]:
+    sum = 0
+    min = l[0]
+    max = l[0]
+    for v in l:
+        sum += v
+        if v > max:
+            max = v
+        elif v < min:
+            min = v
+    return min, max, sum / len(l)
 
-
-a, b = toto()
-print(a, b)
-
-# TP
-# Faire la fonction min_max_avg(l: list[float]) -> tuple[float, float, float]
-# En 1 seule iteration calcul le min, max, avg
+if __name__ == '__main__':
+    l = [1, 99, 50, -1, 23, 45, 7, 11, 9, 10]
+    min, max, avg = min_max_avg(l)
+    print(min, max, avg)
