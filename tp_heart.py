759bd322f6e2484801a99bf15a0dc0d6c2838b04
cyrilvincent <contact@cyrilvincent.com>
2026-02-25 13:30:40 +0100

    CEA

--- tp_heart.py
+++ tp_heart.py
@@ -3,3 +3,18 @@
 # ko dataframe pour num == 1
 # afficher describe pour le dataframe, ok et ko
 # L'age, le genre, le chol la pression (thalach) ont ils un lien avec le diagnostique
+
+import pandas as pd
+import numpy as np
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', None)
+
+dataframe = pd.read_csv("data/heart/data_cleaned_up.csv")
+print(dataframe.describe())
+ok = dataframe[dataframe["num"] == 0]
+ko = dataframe[dataframe["num"] == 1]
+print(ok.describe())
+print(ko.describe())
+print(dataframe.corr())
