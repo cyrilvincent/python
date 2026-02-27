f45cbb3c87bf50bfe98e565d4754329c3f5bdfdd
cyrilvincent <contact@cyrilvincent.com>
2026-02-27 09:55:54 +0100

    CEA

--- demo_qt.py
+++ demo_qt.py
@@ -1,6 +1,7 @@
 from PyQt6.QtCore import QSize
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLabel, QLineEdit, QVBoxLayout
 import sys
+import demo_function
 
 
 class MainWindow(QMainWindow):
@@ -8,9 +9,41 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Formation Python")
-        button = QPushButton("OK")
-        self.setCentralWidget(button)
+        self.label = QLabel("Du texte")
+        self.errorLabel = QLabel()
+        self.setVisible(False)
+        self.edit = QLineEdit()
+        self.button = QPushButton("OK")
+        # self.setCentralWidget(self.button)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.edit)
+        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.errorLabel)
+
+        container = QWidget()
+        container.setLayout(self.layout)
+        self.setCentralWidget(container)
+
+
         self.setMinimumSize(QSize(640,480))
+        self.button.clicked.connect(self.button_clicked)
+        # self.edit.textChanged.connect(self.button_clicked)
+        self.nb = 0
+
+    def button_clicked(self):
+        self.nb += 1
+        # self.button.setText(f"Clicked {self.nb}")
+        # self.label.setText(f"Vous avez saisi {self.edit.text()} {self.nb}")
+        try:
+            nb = int(self.edit.text())
+            result = demo_function.factorielle(nb)
+            self.label.setText(f"{nb}!={result}")
+            self.errorLabel.setVisible(False)
+        except ValueError as ex:
+            self.errorLabel.setText(str(ex))
+            self.errorLabel.setVisible(True)
 
 
 app = QApplication(sys.argv)
