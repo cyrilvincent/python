d8ad5c0a07909307c05f101102730f42c83780a4
Cyril Vincent <contact@cyrilvincent.com>
2026-03-25 11:55:10 +0100

    CNRS

---
+++ tp1.py
@@ -0,0 +1,7 @@
+# Créer la variable year = 2026
+# Afficher le texte "L'année en cours est year"
+# Créer la variable my_text = "Je suis en majuscule"
+# Afficher my_text en majuscule # .upper()
+# Saisir votre année de naissance en affichant un prompt compréhensible par un humain
+# Caster votre année de naissance dans une nouvelle variable avec la fonction int()
+# Afficher votre age en fin d'année
