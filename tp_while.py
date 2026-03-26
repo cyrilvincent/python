147752786be08a79a7679b8a5c113651457f6e32
Cyril Vincent <contact@cyrilvincent.com>
2026-03-26 10:14:13 +0100

    CNRS

--- tp_while.py
+++ tp_while.py
@@ -1,6 +1,6 @@
 # Afficher tous les nombres pairs < 100
 # Décrémenter de 100 à 0 ave un pas de -6
-# Afficher i**2 pour tous les i <= 16
+# Afficher 2**i pour tous les i <= 16
 # Soit n=5, calculer n! 5!=5*4*3*2*1=120
 
 # i = 0
