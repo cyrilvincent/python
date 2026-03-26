9a8674cb4293c6509cc903ce1ab758b65127fb9c
Cyril Vincent <contact@cyrilvincent.com>
2026-03-26 11:26:25 +0100

    CNRS

---
+++ tp_function.py
@@ -0,0 +1,4 @@
+# faire la fonction power(x, y) => x ** y
+# Faire la fonction facto(n: int) -> int
+# Faire la fonction is_prime(n: int) -> bool
+# Bonus faire la fonction qui affiche tous les nb premiers < 1000
