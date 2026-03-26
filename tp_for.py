b68dc806646d7b1341f1320f493af5f1310d0346
Cyril Vincent <contact@cyrilvincent.com>
2026-03-26 09:28:52 +0100

    CNRS

---
+++ tp_for.py
@@ -0,0 +1,4 @@
+# Refaire de tp_while mais avec un for
+# Bonus : soit n=7 est que n est premier
+# Un nb est premier ssi nb>0 il possède exactement 2 diviseurs 1 et n
+# Tout nb>1 est premier sauf s'il possède un diviseur entre 2 et n-1
--- tp_while.py
+++ tp_while.py
@@ -2,3 +2,25 @@
 # Décrémenter de 100 à 0 ave un pas de -6
 # Afficher i**2 pour tous les i <= 16
 # Soit n=5, calculer n! 5!=5*4*3*2*1=120
+
+# i = 0
+# while i < 100:
+#     print(i)
+#     i += 2
+
+# i = 100
+# while i >= 0:
+#     print(i)
+#     i -= 6
+
+# i = 0
+# while i <= 16:
+#     print(2 ** i)
+#     i += 1
+
+n = 5
+i = 1
+while n > 1:
+    i = i * n
+    n -= 1
+print(i)
