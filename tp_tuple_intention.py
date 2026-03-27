ead1e6481a0b305c94d221affe6b601a352e110b
Cyril Vincent <contact@cyrilvincent.com>
2026-03-27 13:13:11 +0100

    CNRS

---
+++ tp_tuple_intention.py
@@ -0,0 +1,6 @@
+# Refaire depuis tp_list les fonction square, filter_even, filter_prime
+# Faire la fonction min_max_avg(l: list[float]) -> tuple[float, float, float]
+# min, max, avg = min_max_avg([1,2,3,4])
+# assert min == 1
+# assert max = 4
+# assert avg = 2.5
