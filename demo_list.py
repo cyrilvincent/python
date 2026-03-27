2528519515a9c7543a44993c43cf6a869344ece8
Cyril Vincent <contact@cyrilvincent.com>
2026-03-27 09:27:22 +0100

    CNRS

--- demo_list.py
+++ demo_list.py
@@ -1,17 +1,52 @@
 l1 = [10,8,77,5,-2,58,99]
-print(l1)
-for value in l1:
-    print(value)
-print(l1[2])
-print(l1[-1])
-print(l1[1:6])
-print(l1[1:-1])
-print(l1[2:-2])
-index = l1.index(58)
-print(index)
-l1.append(999)
-print(l1)
-l1.remove(99)
-print(l1)
-l1[1]=888
-print(l1)
+# print(l1)
+# for value in l1:
+#     print(value)
+# print(l1[2])
+# print(l1[-1])
+# print(l1[1:6])
+# print(l1[1:-1])
+# print(l1[2:-2])
+# index = l1.index(58)
+# print(index)
+# l1.append(999)
+# print(l1)
+# l1.remove(99)
+# print(l1)
+# l1[1]=888
+# print(l1)
+
+for i in range(len(l1)):
+    print(l1[i])
+
+
+
+def sum(l: list[float]) -> float:
+    """
+    sum
+    :param l: blah blah
+    :return:
+    """
+    total = 0
+    for value in l:
+        total += value
+    return total
+
+def clone(l: list[float]) -> list[float]:
+    new_list = []
+    for value in l:
+        new_list.append(value)
+    return new_list
+
+def double(l: list[float]) -> list[float]:
+    new_list = []
+    for value in l:
+        new_list.append(value * 2)
+    return new_list
+
+
+if __name__ == '__main__':
+    assert sum([1,2,3]) == 6
+    assert sum([]) == 0
+    assert clone([1,2,3]) == [1,2,3]
+    assert double([1,2,3]) == [2,4,6]
---
+++ tp_list.py
@@ -0,0 +1,9 @@
+# Dans des fonctions + tests
+# sum
+# multiply
+# min & max
+# square [1,2,3] => [1,4,9]
+# filter_even : filtre que les pairs [1,2,3,4] => [2,4]
+# Bonus : filter_prime [1,2,3,4,5] => [2,3,5]
+# Bonus : refaire la fonction sum par index
+# Bonus : fibo(5) = fibo(n) = fibo(n-1) + fibo(n-2)
