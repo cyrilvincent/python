566b0741b0e518ba475b42fda545c62621800dec
cyrilvincent <contact@cyrilvincent.com>
2026-03-30 15:03:14 +0200

    CEA

---
+++ tp_while.py
@@ -0,0 +1,5 @@
+# Afficher les nb pairs < 100
+# Refaire par vous même la table de multiplication
+# Soit un n un nombre entier par exmple n=5 calculer sa factorielle 5!=5*4*3*2*1
+# Difficile : Soit n=5 calculer fibo(5) Wiki fibonacci
+# Difficle++ : Dire si n est premier ou non : Tout nb > 1 est premier sauf s'il possède un diviseur entre 2 et nb-1
