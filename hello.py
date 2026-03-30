702050f645f825ed84e6aeddbd961b5581d6d6a9
cyrilvincent <contact@cyrilvincent.com>
2026-03-30 11:34:58 +0200

    CEA

---
+++ hello.py
@@ -0,0 +1,28 @@
+print("Hello World")
+print(3+4)
+x = 3
+y = 4
+z = x + y
+print(z)
+
+# int
+x = 3
+# float = real
+y = 3.14
+z = x + y
+root2 = 2 ** 0.5
+print(root2 ** 2)
+
+# s = "3"
+# x = 3
+# print(x + s)
+
+s = "toto"
+print(len(s))
+
+# s = input("Veuillez saisir du texte: ")
+# print(s.upper())
+
+nb = input("Saisir un nombre: ")
+nb2 = int(nb)
+print(f"Le résultat est {nb2 + 1:.2f}")
---
+++ tp1.py
@@ -0,0 +1,6 @@
+#Demander à l'utilisteur de saisir son prénom
+#Afficher le prénom en majuscule
+#Afficher le nombre de caractère du prénom
+#Créer la variable year = 2026
+#Saisir votre année de naissance
+#Afficher votre age en fin d'année
