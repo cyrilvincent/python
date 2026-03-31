ce838221c239b6f4ad26387efc835a60c33008f5
cyrilvincent <contact@cyrilvincent.com>
2026-03-31 14:25:47 +0200

    CEA

--- tp1.py
+++ tp1.py
@@ -5,15 +5,22 @@
 #Saisir votre année de naissance
 #Afficher votre age en fin d'année
 
-prenom = input("Prénom: ")
-print(prenom.upper())
-print(len(prenom))
+# prenom = input("Prénom: ")
+# print(prenom.upper())
+# print(len(prenom))
 
-year=2026
-birth = input("Année de naissance: ")
-birth2 = int(birth)
-age = year - birth2
-print(f"Vous avez {age} ans")
+while True:
+    try:
+        year=2026
+        birth = input("Année de naissance: ")
+        birth2 = int(birth)
+        age = year - birth2
+        if age < 0:
+            raise ValueError("Age < 0")
+        print(f"Vous avez {age} ans")
+        break
+    except ValueError as ex:
+        print(f"Erreur {ex}")
 
 # birth n'est pas un nb
 # age ne peut pas être < 0
