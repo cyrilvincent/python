4ffdbb5f34f2e9f142f95bc65e065ebf1460f47e
cyrilvincent <contact@cyrilvincent.com>
2026-04-01 11:40:32 +0200

    CEA

--- tp_tuple.py
+++ tp_tuple.py
@@ -1,2 +1,15 @@
 def min_max_mean(list: list[float]) -> tuple[float, float, float]:
-    #+ test
+    sum = 0
+    min = list[0]
+    max = list[0]
+    for value in list:
+        sum += value
+        if value < min:
+            min = value
+        elif value > max:
+            max = value
+    return min, max, sum / len(list)
+
+if __name__ == '__main__':
+    min, max, mean = min_max_mean([0,1,2,3,4,5,6,7,8,9])
+    print(min, max, mean)
