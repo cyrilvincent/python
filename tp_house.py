70680a80078a3ff36566ecc611f36d9b5ac80131
cyrilvincent <contact@cyrilvincent.com>
2026-04-02 11:45:58 +0200

    CEA

--- tp_house.py
+++ tp_house.py
@@ -12,6 +12,8 @@ print(loyers)
 # Filtrer les surfaces > 200
 # Filtrer les loyers dont la surface > 200
 
+# Afficher le nuage de points x = surfaces et y = loyers
+
 print(np.min(loyers), np.max(loyers), np.mean(loyers), np.std(loyers), np.median(loyers))
 loyer_m2 = loyers / surfaces
 print(np.mean(loyer_m2), np.median(loyer_m2))
