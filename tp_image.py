d2f96677af0191d776965764abb1cd576d0700bf
cyrilvincent <contact@cyrilvincent.com>
2026-04-02 11:16:46 +0200

    CEA

--- tp_image.py
+++ tp_image.py
@@ -3,14 +3,42 @@
 from PIL import Image
 import numpy as np
 
-im = Image.open("data/ski.jpg")
-array = np.asarray(im).astype(np.float64)
+def load(path: str):
+    im = Image.open(path)
+    return np.asarray(im).astype(np.float64)
 
-array = array[100:-100]
-red = array[:,:,0]
+def save(array, path: str):
+    dest = Image.fromarray(array.astype(np.uint8)).convert("RGB")
+    dest.save(path)
 
-dest = Image.fromarray(red.astype(np.uint8)).convert("RGB")
-dest.save("data/out.png")
+def get_chanel(array, num: int):
+    return array[:,:,num]
+
+def crop(array, north, south, east, west):
+    return array[north:-south, east:-west]
+
+def lum(array):
+    return np.mean(array)
+
+def contrast(array):
+    return np.std(array)
+
+def reduce(array, factor):
+    return array[::factor, ::factor]
+
+def auto_normalize(array):
+    mean = lum(array)
+    std = contrast(array)
+    return np.clip(((array - mean) / std) * 255 / 4 + 255 / 2, 0 ,255)
+
+if __name__ == '__main__':
+    array = load("data/ski.jpg")
+    green = get_chanel(array, 1)
+    cropped = crop(green, 100,200,300,400)
+    print(f"Luminance: {lum(array):.1f}, contrast: {contrast(array):.1f}")
+    reduced = reduce(array, 2)
+    norm = auto_normalize(array)
+    save(norm, "data/out.png")
 
 # Créer la fonction load(path) -> array
 # save(array, path)
@@ -21,3 +49,4 @@ dest.save("data/out.png")
 # reduce(array, factor) reduce(array, 2) réduit l'image de 4
 # auto_normalize : Bonus ((x - mean) / std) * 255/4 + 255/2
 # np.clip(x,0,255)
+
