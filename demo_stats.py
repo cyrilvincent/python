d57959d9d9420de65bd283a4f28b63474555049d
Cyril Vincent <contact@cyrilvincent.com>
2026-04-19 19:45:28 +0200

    CEA

--- demo_stats.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-rnd = np.random.rand(1000)
-print(np.mean(rnd), np.median(rnd), np.std(rnd), np.quantile(rnd, [0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.99]))
--- demo_zip.py
+++
@@ -1,6 +0,0 @@
-tensions = [220,220,221,222,223]
-courants = [1,1.1,1.2,1.3,1.1]
-
-for t, a in zip(tensions, courant):
-    power = t * a
-    print(power)
