8574ff5595fd7ed3c233588d74b08ff68d0401e7
cyrilvincent <contact@cyrilvincent.com>
2026-04-20 11:13:10 +0200

    CEA

---
+++ hello.py
@@ -0,0 +1,30 @@
+print("Hello World!")
+
+print(3**2)
+
+a=3**2
+print(a)
+
+x = 3
+y = "3"
+print(x, y)
+print(type(x))
+print(type(y))
+# print(x + y)
+print(x + 3)
+print(y + "3")
+print(len("toto"))
+
+s = "toto"
+l = len(s)
+print(l)
+
+print(s.count("o"))
+
+
+age = float(input("Age: "))
+# age2 = int(age)
+age = age + 1
+
+print(f"Vous avez {age:.2f} ans")
+
---
+++ tp1.py
@@ -0,0 +1,5 @@
+#Créer la variable year=2026
+#Saisir votre prénom
+#Afficher Bonjour + votre prénom en majuscule
+#Saisir et stocker votre année de naissance
+#Calculer et afficher votre age en fin d'année
