0d5e96e60e03b7c75ee8ed988a428dec8c77c3e6
cyrilvincent <contact@cyrilvincent.com>
2026-04-20 14:54:03 +0200

    CEA

--- tp_retraite.py
+++ tp_retraite.py
@@ -6,3 +6,30 @@
 # Saisir votre age de début de cotisation
 # En supposant que vous ne serez jamais au chomage et jamais enceinte calculer votre age de départ à la retraite
 # A 67 ans vous êtes à taux plein
+
+birth_year = 1972
+
+if birth_year <= 1964:
+    age_legal = 62
+elif birth_year <= 1968:
+    age_legal = 63
+else:
+    age_legal = 64
+
+print(f"Age légal: {age_legal}")
+
+if birth_year <= 1965:
+    nb_trimestre = 170
+elif birth_year <= 1966:
+    nb_trimestre = 171
+else:
+    nb_trimestre = 172
+
+age_cotisation = 23
+
+age_depart = age_cotisation + nb_trimestre / 4
+if age_depart > 67:
+    age_depart = 67
+
+print(f"Age départ: {age_depart}")
+
