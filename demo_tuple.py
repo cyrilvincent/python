12361b6863c7c58b082e59d5403f5086f2c356c3
cyrilvincent <contact@cyrilvincent.com>
2026-04-22 14:39:32 +0200

    CEA

--- demo_tuple.py
+++ demo_tuple.py
@@ -1,6 +1,14 @@
 def min_max_avg(l: list[float]) -> tuple[float, float, float]:
-
-    return 0,10,5
+    total = 0
+    min = l[0]
+    max = l[0]
+    for value in l:
+        total += value
+        if value < min:
+            min = value
+        elif value > max:
+            max = value
+    return min, max, total / len(l)
 
 min, max, avg = min_max_avg([1,2,3,4])
 print(min, max, avg)
