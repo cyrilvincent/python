9e17640762551eae3af874370f042ee03926922e
cyrilvincent <contact@cyrilvincent.com>
2026-04-22 13:50:12 +0200

    CEA

---
+++ demo_ref.py
@@ -0,0 +1,23 @@
+# type by value
+a = 1
+b = a
+a += 1
+print(a, b)
+
+# type by reference
+a = [1, 2]
+b = a
+a.append(3)
+print(a, b)
+
+# clonage
+a = [1, 2]
+b = list(a)
+a.append(3)
+print(a, b)
+
+a = [1, 2]
+b = [1, 2]
+print(a == b, a is b)
+a = b
+print(a == b, a is b)
