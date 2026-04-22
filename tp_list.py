ff41dff75205e079217dab536bdaeb9556ba23d9
cyrilvincent <contact@cyrilvincent.com>
2026-04-22 09:59:39 +0200

    CEA

---
+++ tp_list.py
@@ -0,0 +1,10 @@
+l = [1,2,5,-5,55,99,0,-3,4,77]
+
+def sum(l: list[float]) -> float:
+    pass
+
+# def mean
+# def min
+# def max
+# difficile : filter_even([1,2,3,4]) -> [2,4]
+# +difficile : filter_prime([1,2,3,4,5]) -> [2,3,5]
