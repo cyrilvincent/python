079b1d3f0a1095040e0c09ebfd5e8a9c51f84662
cyrilvincent <contact@cyrilvincent.com>
2026-04-23 14:42:59 +0200

    CEA

--- tp_heart.py
+++ tp_heart.py
@@ -4,3 +4,16 @@
 # afficher un describe pour ok & ko
 # que peut on en déduire sur age, sex, trestbps, thalach et chol
 # tester df.corr() = Pearson
+
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+df = pd.read_csv("data/heart/data_cleaned_up.csv")
+ok = df[df["num"] == 0]
+ko = df[df["num"] == 1]
+
+print(ok.describe())
+print(ko.describe())
+
+print(df.corr(method="pearson"))
