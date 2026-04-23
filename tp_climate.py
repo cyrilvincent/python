e427e271e3f2f093595fa07cdda9df52d3d1a0f6
cyrilvincent <contact@cyrilvincent.com>
2026-04-23 14:10:26 +0200

    CEA

--- tp_climate.py
+++ tp_climate.py
@@ -1,3 +1,21 @@
 # Charger dans un df data/climate/jena_filtered.csv
 # min, max mean sur T (degC)
 # Afficher en x Date Time et y = T (degC) dans un plot
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+df = pd.read_csv("data/climate/jena_filtered.csv")
+
+df = df[12::24]
+print(df.describe())
+
+df_filtered = df[(df["T (degC)"] <= 0) & (df["rh (%)"] >= 50)]
+
+x = df_filtered["Date Time"]
+y = df_filtered["T (degC)"]
+plt.plot(x, y)
+plt.show()
---
+++ tp_heart.py
@@ -0,0 +1,6 @@
+# Lire dans un df data/heart/data_cleaned_up.csv
+# Créer le df ok pour les num==0
+# Créer le df ko pour les num==1
+# afficher un describe pour ok & ko
+# que peut on en déduire sur age, sex, trestbps, thalach et chol
+# tester df.corr() = Pearson
