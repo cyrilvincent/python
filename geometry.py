8c14c154ae84c4edea14e618a28349b098256fd1
cyrilvincent <contact@cyrilvincent.com>
2026-04-24 14:57:43 +0200

    CEA

--- geometry.py
+++ geometry.py
@@ -1,8 +1,23 @@
+class Point:
+
+    def __init__(self,x , y):
+        self.x = x
+        self.y = y
+
+    def move(self,x, y):
+        self.x = x
+        self.y = y
+
+    def move_rel(self, x, y):
+        self.x += x
+        self.y += y
+
 class Rectangle:
 
-    def __init__(self, width, length):
+    def __init__(self, width: float, length, origin: Point):
         self.width = width
         self.length = length
+        self.origin = origin
 
     def area(self):
         return self.width * self.length
@@ -11,7 +26,11 @@ class Rectangle:
         return 2 * (self.width + self.length )
 
 if __name__ == '__main__':
-    r1 = Rectangle(3,2)
-    r2 = Rectangle(4,6)
+    p1 = Point(2,2)
+    r1 = Rectangle(3,2, p1)
+    r2 = Rectangle(4,6, Point(4, -1))
     print(r1.area())
     print(r2.perimeter())
+    print(r1.origin.x)
+    r1.origin.move_rel(1,1)
+    print(r1.origin.x)
--- tp_oo.py
+++ tp_oo.py
@@ -1,10 +1,29 @@
 class Atom:
 
-    pass
+    def __init__(self, nb_electron, nb_proton, nb_neutron, period, symbol):
+        self.nb_electron = nb_electron
+        self.nb_proton = nb_proton
+        self.nb_neutron = nb_neutron
+        self.period = period
+        self.symbol = symbol
 
-    # oxyder, reduire
+    def ox(self, n: int):
+        self.nb_electron -= n
 
-class Book:
-    # Environ 4 à 7 attributs
-    # net_price 5.5%
-    pass
+    def red(self, n: int):
+        self.nb_electron += n
+
+    def masse_atom(self):
+        return self.nb_proton + self.nb_neutron
+
+    def rayon_atom(self):
+        return 1.2 * self.masse_atom() ** 1/3
+
+if __name__ == '__main__':
+    fer = Atom(26,26,26,4,"Fe")
+    assert fer.masse_atom() == 52
+    assert fer.nb_electron == 26
+    fer.ox(2)
+    assert fer.nb_electron == 24
+    fer.red(2)
+    assert fer.nb_electron == 26
