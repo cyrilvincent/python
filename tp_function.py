ba284d66cafd9373d32dcfcec8a3f62f91840a10
Cyril Vincent <contact@cyrilvincent.com>
2026-05-04 11:27:21 +0200

    Radiall

---
+++ tp_function.py
@@ -0,0 +1,5 @@
+# Ecrice la fonction multiply(x, y) qui multiplie x et y + pydoc
+# def factorielle(n: int) -> int 5! = 1*2*3*4*5 = 120
+# def is_prime(n: int) -> bool Tout nb > 1 est premier sauf s'il possède un diviseur entre 2 et nb-1
+# Bonus : fibonacci f(n) = (n-1) f(n-2) avec f0 = 0 et f1=1
+
