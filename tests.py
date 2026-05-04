d8c67fe16482ead9abb38721245e52fc1f58417f
Cyril Vincent <contact@cyrilvincent.com>
2026-05-04 15:22:28 +0200

    Radiall

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 import unittest
 import tp_function
 import math
+import tp_list
+
 
 class MyTests(unittest.TestCase):
 
@@ -24,3 +26,19 @@ class MyTests(unittest.TestCase):
         with self.assertRaises(ValueError):
             tp_function.factorielle(-1)
 
+
+    def test_sum(self):
+        result = tp_list.sum([1,2,3,4])
+        self.assertEqual(10, result)
+
+    def test_filter_even(self):
+        result = tp_list.filter_even([1,2,3,4])
+        self.assertEqual([2,4], result)
+
+    def test_filter_prime(self):
+        result = tp_list.filter_prime([1,2,3,4,5,6,7])
+        self.assertEqual([2,3,5,7], result)
+
+    def test_remove_all(self):
+        result = tp_list.remove_all([1,333,2,333,3,333], 333)
+        self.assertEqual([1,2,3], result)
---
+++ tp_list.py
@@ -0,0 +1,10 @@
+l = [1,2,3,55,99,-2,4,8,-3,1000]
+for v in l:
+    print(v)
+
+for i in range(len(l)):
+    print(l[i])
+
+for i, v in enumerate(l):
+    print(i, v, l[i])
+
