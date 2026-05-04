07bcd60a1899e4a10a6528b50fef6e35257c9ceb
Cyril Vincent <contact@cyrilvincent.com>
2026-05-04 15:24:03 +0200

    Radiall

--- tp_list.py
+++ tp_list.py
@@ -1,10 +1,15 @@
-l = [1,2,3,55,99,-2,4,8,-3,1000]
-for v in l:
-    print(v)
+# l = [1,2,3,55,99,-2,4,8,-3,1000]
+# for v in l:
+#     print(v)
+#
+# for i in range(len(l)):
+#     print(l[i])
+#
+# for i, v in enumerate(l):
+#     print(i, v, l[i])
 
-for i in range(len(l)):
-    print(l[i])
+def sum(l):
+    pass
 
-for i, v in enumerate(l):
-    print(i, v, l[i])
-
+def filter_even(l):
+    pass
