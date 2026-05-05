04843a1c3fab98c367c4814fd474393654c455f4
Cyril Vincent <contact@cyrilvincent.com>
2026-05-05 09:39:35 +0200

    Radiall

--- demo_tuple.py
+++ demo_tuple.py
@@ -1,9 +1,15 @@
 def min_max_avg(l: list[int]) -> tuple[int, int, float]:
-    min = 0
-    max = 99
-    sum = 500
-    return min, max, sum/len(l)
+    total = 0
+    min = l[0]
+    max = l[0]
+    for v in l:
+        total += v
+        if v < min:
+            min = v
+        elif v > max:
+            max = v
+    return min, max, total / len(l)
 
 
-min, max, avg, toto = min_max_avg([1,2,3,4,5,6])
+min, max, avg = min_max_avg([1,2,3,4,5,6])
 print(min, max, avg)
