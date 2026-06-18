ecb3c340cdf1f526f65e874a032ddfeb2581afc3
Cyril Vincent <contact@cyrilvincent.com>
2026-06-18 09:20:15 +0200

    poma

--- tp_heart.py
+++
@@ -1,6 +0,0 @@
-# Lire data/heart/data_cleaned_up.csv
-
-# Créer le df ok pour num==0
-# Créer le df ko pour num==1
-# Afficher ok.describe() et ko.describe() que voyez vous pour age?, chol, sex, talach!
-# Confirmer les résultat par la corr()
