a3e9fef03cb1d2110c96afa8c478ef0db8115bff
user92 <user92@atp-formation.local>
2026-06-18 17:05:53 +0200

    poma

--- demo_exception.py
+++ demo_exception.py
@@ -1,11 +1,13 @@
-try:
-    age = input("Age : ")
-    age = int(age)
-except ValueError as ex:
-    print(f"Erreur valeur : {ex}")
-except TypeError as ex:
-    print(f"Erreur type : {ex}")
-except Exception as ex:
-    print(f"Unknow error : {ex}")
-
+while True:
+    try:
+        age = input("Age : ")
+        age = int(age)
+        print(f"Vous avez {age} ans")
+        break
+    except ValueError as ex:
+        print(f"Erreur valeur : {ex}")
+    except TypeError as ex:
+        print(f"Erreur type : {ex}")
+    except Exception as ex:
+        print(f"Unknow error : {ex}")
 
