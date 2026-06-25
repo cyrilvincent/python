8fa33c2da9c5437db2378fd7f177ea4e1667553a
user92 <user92@atp-formation.local>
2026-06-25 11:39:45 +0200

    atp

--- demo_tuple.py
+++ demo_tuple.py
@@ -2,10 +2,16 @@ p1: tuple[float, float, float] = (3,2,-1)
 print(p1)
 
 def min_max_avg(l: list[int]) -> tuple[int, int, float]:
-    min = 0
-    max = 99
-    avg = 50
-    return min, max, avg
+    sum = 0
+    min = l[0]
+    max = l[0]
+    for v in l:
+        sum += v
+        if v < min:
+            min = v
+        elif v > max:
+            max = v
+    return min, max, sum / len(l)
 
 l = [1,2,3,99,4,5,98,6,7,8,9]
 min, max, avg = min_max_avg(l)
