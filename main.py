578cea85cbb4e5058463c64daa3ccbb5f43f7ff9
user72 <user72@atp-formation.local>
2026-06-29 14:12:01 +0200

    atp

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-import tp_function as lib
+import sys
+from PyQt6.QtWidgets import QMainWindow, QFileDialog
+from main_window import Ui_MainWindow
+from PyQt6 import QtWidgets
 
-print(lib.factorielle(5))
+
+class MyApp(QMainWindow, Ui_MainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.pushButton.clicked.connect(self.pushButton_clicked)
+        self.show()
+
+    def pushButton_clicked(self):
+        value = self.lineEdit.text()
+        self.label.setText(f"H {value}")
+
+   
+
+app = QtWidgets.QApplication(sys.argv)
+window = MyApp()
+app.exec()
--- tp_heart.py
+++ tp_heart.py
@@ -4,3 +4,23 @@
 # Créer ok = pour num==0
 # Créer ko = pour num==1
 # Afficher ok.Describe() et ko.describe() qu'en déduire
+# Sauvegarder ok en xlsx
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+df = pd.read_csv("data/heart/data_cleaned_up.csv")
+
+ok = df[df["num"] == 0]
+ko = df[df["num"] == 1]
+
+print(ok.describe())
+print(ko.describe())
+
+plt.scatter(ok["age"], ok["chol"], color="green")
+plt.scatter(ko["age"], ko["chol"], color="red")
+plt.show()
+
