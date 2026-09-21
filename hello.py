7fabc28f2e058a86bbe90fe2bb910825d62e3859
cyrilvincent <contact@cyrilvincent.com>
2026-09-21 10:45:26 +0200

    CEA

--- hello.py
+++ hello.py
@@ -1,5 +1,26 @@
-print("Hello World!")
+print("Hello'\t World!")
 a = 1
 b = a + 1
 print(b)
+
+i = 0
+i = i + 1
+i += 1
+
+print(3//2, 3%2)
+
+x = 3.14
+i= 1
+j = x + i
+print(type(x))
+
+# nb = input("Saisir nb: ")
+# nb = int(nb)
+# print(nb + 1)
+# s = "toto"
+# s.lower()
+
+nb = 3.14116
+print(f"Le résultat du calcul est {nb:.2d}")
+
 
---
+++ tp1.py
@@ -0,0 +1,3 @@
+# Créer la variable actual_year = 2026
+# Saisir votre année de naissance
+# Afficher votre age en fin d'année
