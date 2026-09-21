f69c46751ea27504f84206ef0d20d883d52ec9a4
cyrilvincent <contact@cyrilvincent.com>
2026-09-21 14:06:56 +0200

    CEA

---
+++ demo_func.py
@@ -0,0 +1,8 @@
+def add(i: float, j: float) -> float:
+    return i + j
+
+print(add(2, 3))
+result = add(2, 3)
+print(result)
+print(add(j=2, i=3))
+print(add(2, 3.14))
---
+++ tp_function.py
@@ -0,0 +1,2 @@
+# Porter factorielle, fibo et is_prime en fonction
+# Bonus : typer les fonctions def factorielle(nb : int) -> int
