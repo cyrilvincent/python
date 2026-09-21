21cf5eff4497e08c47aeea5e16fb6b00e10a8526
cyrilvincent <contact@cyrilvincent.com>
2026-09-21 10:53:47 +0200

    CEA

--- tp1.py
+++ tp1.py
@@ -1,3 +1,9 @@
 # Créer la variable actual_year = 2026
 # Saisir votre année de naissance
 # Afficher votre age en fin d'année
+
+actual_year = 2026
+birth_year = input("Année de naissance: ")
+birth_year = int(birth_year)
+age = actual_year - birth_year
+print(f"Vous avez {age} ans")
