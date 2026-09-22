2681a301e2656c042fa7ecbdae714c6c820ded4e
cyrilvincent <contact@cyrilvincent.com>
2026-09-22 10:07:49 +0200

    CEA

--- tp_list.py
+++ tp_list.py
@@ -4,3 +4,44 @@
 # Créer la fonction max(l)
 # Créer la fonction filter_even(l) -> list[int] : [1,2,3,4] => [2,4]
 # Créer la fonction filter_prime(l) : [1,2,3,4,5] => [2,3,5]
+import tp_function
+
+def display(l: list[int]):
+    for v in l:
+        print(v)
+
+def sum(l: list[int]) -> int:
+    total = 0
+    for v in l:
+        total += v
+    return total
+
+def max(l: list[int]) -> int:
+    max = l[0]
+    for v in l[1:]:
+        if v > max:
+            max = v
+    return max
+
+def filter_even(l: list[int]) -> list[int]:
+    result = []
+    for v in l:
+        if v % 2 == 0:
+            result.append(v)
+    return result
+
+def filter_prime(l: list[int]) -> list[int]:
+    result = []
+    for v in l:
+        if tp_function.is_prime(v):
+            result.append(v)
+    return result
+
+
+if __name__ == '__main__':
+    l = [1, 2, 3, 8, 9, -1, 99, 0, 66, 88]
+    display(l)
+    assert sum(l) == 275
+    assert max(l) == 99
+    assert filter_even(l) == [2,8,0,66,88]
+    assert filter_prime(l) == [2,3]
