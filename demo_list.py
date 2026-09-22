9e3480cf1c18be6adafc0ec7442d31dea081b2e9
cyrilvincent <contact@cyrilvincent.com>
2026-09-22 09:16:40 +0200

    CEA

---
+++ demo_list.py
@@ -0,0 +1,25 @@
+l = [1,2,3,8,9,-1,99,0,66]
+print(l[0])
+print(l[1:5])
+print(l[2:-2])
+print(l[:5])
+print(l[5:])
+
+l.append(33)
+l.insert(4,100)
+l.remove(8)
+print(l)
+print(l.count(99))
+
+for value in l:
+    print(value)
+
+for i in range(len(l)):
+    print(l[i])
+
+l = []
+i = 0
+while i < 10:
+    l.append(i)
+    i += 1
+print(l)
---
+++ tp_list.py
@@ -0,0 +1,6 @@
+# Créer une liste l avec 10 éléments non consécutifs "en dur" l = [1,2,3,8,9,-1,99,0,66]
+# Créer la fonction display(l: list[int]) => print
+# Créer la fonction sum(l) -> int
+# Créer la fonction max(l)
+# Créer la fonction filter_even(l) -> list[int] : [1,2,3,4] => [2,4]
+# Créer la fonction filter_prime(l) : [1,2,3,4,5] => [2,3,5]
