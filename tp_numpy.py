759917579be2eb682a00db35b8a3bd5cce3c5f04
cyrilvincent <contact@cyrilvincent.com>
2026-09-23 15:17:31 +0200

    CEA

--- tp_numpy.py
+++ tp_numpy.py
@@ -10,3 +10,8 @@ loyers = data["loyers"]
 # Afficher les surfaces > 200
 # Afficher les loyers dont la surface > 200
 
+loyer_m2 = surfaces / loyers
+print(np.min(surfaces), surfaces.max())
+filter = surfaces > 200
+print(surfaces[filter])
+print(loyers[filter])
