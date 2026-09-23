d3af62ba858d2ae54cc94c8d9e30cd6517bca1b0
cyrilvincent <contact@cyrilvincent.com>
2026-09-23 13:33:53 +0200

    CEA

--- tp_house.py
+++ tp_house.py
@@ -4,6 +4,8 @@
 # Créer l'attribut loyer_m2 = []
 # Créer la méthode compute_loyer_m2()
 import csv
+import pickle
+import json
 
 class HouseRepository:
 
@@ -25,20 +27,29 @@ class HouseRepository:
         for loyer, surface in zip(self.loyers, self.surfaces):
             self.loyer_m2.append(loyer / surface)
 
-    def save_pickle(self):
-        # sauvegarder les loyers et surfaces
-        # pickle.dump((x, y), f)
-        # pass
+    def save_pickle(self, path):
+        with open(path, "wb") as f:
+            pickle.dump((self.loyers, self.surfaces), f)
 
-        # load_pickle(path)
-        # save_json module json dump(indent=2)
-        # load_json
-        pass
+    def save_json(self, path):
+        with open(path, "w") as f:
+            json.dump((self.loyers, self.surfaces), f, indent=2)
+
+    def load_pickle(self, path):
+        with open(path, "rb") as f:
+            self.loyers, self.surfaces = pickle.load(f)
+    def load_json(self, path):
+        with open(path, "r") as f:
+            self.loyers, self.surfaces = json.load(f)
 
 
 if __name__ == '__main__':
     repo = HouseRepository()
     repo.load("data/house/house.csv")
+    repo.save_pickle("data/house/house.pkl")
+    repo.load_pickle("data/house/house.pkl")
+    repo.save_json("data/house/house.json")
+    repo.load_json("data/house/house.json")
     repo.compute_loyer_m2()
     print(repo.loyer_m2)
 
