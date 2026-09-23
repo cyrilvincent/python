2af936a6a260ad79fe85e767afce1bf4bd07ce5f
cyrilvincent <contact@cyrilvincent.com>
2026-09-23 11:32:46 +0200

    CEA

--- tp_house.py
+++ tp_house.py
@@ -3,3 +3,32 @@
 # Créer la méthode load(path) qui assigne surfaces et loyers
 # Créer l'attribut loyer_m2 = []
 # Créer la méthode compute_loyer_m2()
+import csv
+
+class HouseRepository:
+
+    def __init__(self):
+        self.loyers = []
+        self.surfaces = []
+        self.loyer_m2 = []
+
+    def load(self, path: str):
+        with open(path, "r") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                loyer = int(row["loyer"])
+                surface = int(row["surface"])
+                self.loyers.append(loyer)
+                self.surfaces.append(surface)
+
+    def compute_loyer_m2(self):
+        for loyer, surface in zip(self.loyers, self.surfaces):
+            self.loyer_m2.append(loyer / surface)
+
+
+if __name__ == '__main__':
+    repo = HouseRepository()
+    repo.load("data/house/house.csv")
+    repo.compute_loyer_m2()
+    print(repo.loyer_m2)
+
