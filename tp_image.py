6ebf38dfa6e6df65a4a1b567dc5a94e533e920b1
cyrilvincent <contact@cyrilvincent.com>
2026-09-24 11:43:10 +0200

    CEA

--- tp_image.py
+++ tp_image.py
@@ -1,15 +1,74 @@
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
 
-im = Image.open("data/ski.jpg")
-array = np.asarray(im).astype(np.float64)
-print(array.shape, array.dtype)
+class ImageManagement:
 
-luminance = np.mean(array)
-print(luminance)
+    def load(self, path):
+        im = Image.open(path)
+        array = np.asarray(im).astype(np.float64)
+        return array
 
-red = array[:,:,0]
-print(red.shape)
+    def save(self, array, path):
+        dest = Image.fromarray(array.astype(np.uint8)).convert("RGB")
+        dest.save(path)
+
+    def luminance(self, array):
+        return np.mean(array)
+
+    def contrast(self, array):
+        return np.std(array)
+
+    def get_chanel(self, array, num):
+        return array[:,:,num]
+
+    def crop(self, array, north, south, east, west):
+        return array[north:-south, west:-east]
+
+    def reduce(self, array, nb):
+        step = int(nb ** 0.5)
+        return array[::step, ::step]
+
+
+    def grey(self, array):
+        return np.mean(array, axis=2)
+
+    def negative(self, array):
+        return 255 - array
+
+
+    def profil(self, array, horizontal=True):
+        if horizontal:
+            return np.mean(array, axis=0)
+        else:
+            return np.mean(array, axis=1)
+
+    def normalize(self, array):
+        mean = self.luminance(array)
+        std = self.contrast(array)
+        return np.clip(((array - mean) / std) * 255/4 + 127.5,0 ,255)
+
+
+
+if __name__ == '__main__':
+    im = ImageManagement()
+    array = im.load("data/ski.jpg")
+    print(f"Luminance: {im.luminance(array):.2f}")
+    print(f"Contrast: {im.contrast(array):.2f}")
+    red = im.get_chanel(array, 0)
+    cropped = im.crop(array, 100, 200, 300, 400)
+    reduced = im.reduce(array, 4)
+    grey = im.grey(array)
+    negative = im.negative(array)
+    normalized = im.normalize(array)
+    im.save(normalized, "data/out.png")
+    array = im.load("data/cercle.jpg")
+    profileh = im.profil(array)
+
+    plt.plot(profileh)
+    plt.show()
+
+
 
 # Mettre en fonction, bonus en class
 # class MyImage
@@ -23,5 +82,3 @@ print(red.shape)
 # np.clip((x - mean) / std) * 255/4 + 255/2,0 ,255)
 
 
-dest = Image.fromarray(red.astype(np.uint8)).convert("RGB")
-dest.save("data/out.png")
