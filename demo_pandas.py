c3b22fb00a9b64713602f2e051da5b958e877af5
cyrilvincent <contact@cyrilvincent.com>
2026-09-24 16:09:00 +0200

    CEA

--- demo_pandas.py
+++ demo_pandas.py
@@ -1,4 +1,7 @@
+import numpy as np
 import pandas as pd
+import scipy.optimize as opt
+import matplotlib.pyplot as plt
 # pip install openpyxl
 
 dataframe = pd.read_csv("data/house/house.csv")
@@ -6,3 +9,14 @@ print(dataframe.columns)
 dataframe["loyer_m2"] = dataframe["loyer"] / dataframe["surface"]
 print(dataframe[dataframe["surface"] > 200])
 dataframe.to_excel("data/house/house.xlsx", index=False)
+
+def poly2(x, a, b, c):
+    return a * x ** 2 + b * x + c
+
+x = np.arange(400)
+
+weight, cov = opt.curve_fit(poly2, dataframe["surface"], dataframe["loyer"])
+print(cov)
+plt.scatter(dataframe["surface"], dataframe["loyer"])
+plt.plot(x, poly2(x, weight[0], weight[1], weight[2]), color="red")
+plt.show()
