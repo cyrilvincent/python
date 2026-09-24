6fa066877d8f9ec27677c8678af57645871c8691
cyrilvincent <contact@cyrilvincent.com>
2026-09-24 14:16:57 +0200

    CEA

--- tp_jena.py
+++ tp_jena.py
@@ -1,4 +1,4 @@
 # Charger jena_filtered avec pandas
-# Prendre la temperature à midi [12::24]
+# Prendre la temperature à midi [11::24]
 # Afficher dans un plot
 # Une regression lineaire recuperer la slope * 365
