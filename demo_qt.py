81e528c650868fe084fe8367192658d05a6c9e9e
cyrilvincent <contact@cyrilvincent.com>
2026-09-25 11:07:14 +0200

    CEA

--- demo_qt.py
+++ demo_qt.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QVBoxLayout, QWidget, QLabel
 
 # app = QApplication(sys.argv)
 #
@@ -11,9 +11,22 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Formation Python")
-        button = QPushButton("OK")
-        self.setCentralWidget(button)
+        self.button = QPushButton("OK")
+        self.lineEdit = QLineEdit()
+        self.label = QLabel()
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.lineEdit)
+        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.label)
+        container = QWidget()
+        container.setLayout(self.layout)
+        self.setCentralWidget(container)
+        self.button.clicked.connect(self.button_clicked)
+        self.i = 0
 
+    def button_clicked(self):
+        self.i += 1
+        self.label.setText(f"Clicked {self.i} {self.lineEdit.text()}")
 
 app = QApplication(sys.argv)
 window = MainWindow()
