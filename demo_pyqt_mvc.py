a34b8b171e04e54efed8d90ea034a2c19393cca4
cyrilvincent <contact@cyrilvincent.com>
2026-09-25 14:45:53 +0200

    CEA

--- demo_pyqt_mvc.py
+++ demo_pyqt_mvc.py
@@ -2,6 +2,7 @@ from PyQt6 import QtWidgets, uic
 import sys
 from PyQt6.QtWidgets import QMainWindow, QApplication
 import main_window
+import geometry
 
 # Sauvegarder le MyMainWindow.ui dans ui/
 # python -m PyQt6.uic.pyuic ui/MyMainWindow.ui > main_window.py
@@ -13,6 +14,16 @@ class MainWindow(QMainWindow, main_window.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.pushButton.clicked.connect(self.button_clicked)
+        self.rectangle = geometry.Rectangle(0,0,geometry.Point(0,0))
+
+    def button_clicked(self):
+        width = int(self.lineEdit.text())
+        length = int(self.lineEdit_2.text())
+        self.rectangle.width = width
+        self.rectangle.length = length
+        area = self.rectangle.area()
+        self.label.setText(f"{area}")
 
 
 app = QApplication(sys.argv)
--- demo_qt_ui.py
+++ demo_qt_ui.py
@@ -1,14 +1,12 @@
-from PyQt6 import QtWidgets, uic
 import sys
-from PyQt6.QtWidgets import QMainWindow, QApplication
-
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QVBoxLayout, QWidget, QLabel
+from PyQt6 import uic
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        uic.loadUi("ui/mock.ui", self)
-
+        uic.loadUi("ui/MainWindow.ui", self)
 
 
 app = QApplication(sys.argv)
